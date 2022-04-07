40f078445e1e16fcfe8909863c368b203f46ad4e
BoicaSebi <sebastian@graffino.com>
2022-04-07 14:17:29 +0300

    app configurations

--- src/multisigConfig.ts
+++ src/multisigConfig.ts
@@ -1,5 +1,6 @@
 //add a valid contract address to make it exclusive, leave empty or null for default behaviour
-export const uniqueContractAddress = '';
+export const uniqueContractAddress =
+  'erd1vlj3u8k7h3ua2v6lxkgtn5jw2pu2t4zggxngf95eger0d2e7gwmqlf7a2a';
 
 //Optional, if you want to give a name to the visual representation of the contract
 export const uniqueContractName = 'My one and only multisig';
