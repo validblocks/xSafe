2b1837865118d89535a70f11535115ba63fe20c2
Rares Raulea <rares@graffino.com>
2022-04-08 14:01:48 +0300

    fix: use multiwallet contract address

--- src/multisigConfig.ts
+++ src/multisigConfig.ts
@@ -1,5 +1,6 @@
 //add a valid contract address to make it exclusive, leave empty or null for default behaviour
-export const uniqueContractAddress = '';
+export const uniqueContractAddress =
+  'erd1qqqqqqqqqqqqqpgqettaulcsh6afs9h4mhsv44lu28p0rezehdeqk7nttw';
 
 //Optional, if you want to give a name to the visual representation of the contract
-export const uniqueContractName = 'My one and only multisig';
+export const uniqueContractName = 'Graffino';
