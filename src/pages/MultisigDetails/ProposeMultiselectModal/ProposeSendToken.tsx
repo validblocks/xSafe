6eb48728db7d9f176bf26184f439e1b889d45365
Rares Raulea <rares@graffino.com>
2022-04-21 12:32:11 +0300

    feat(assets): add dropdown with available tokens and nomination before transaction transfer

--- src/pages/MultisigDetails/ProposeMultiselectModal/ProposeSendToken.tsx
+++ src/pages/MultisigDetails/ProposeMultiselectModal/ProposeSendToken.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { operations } from '@elrondnetwork/dapp-utils';
 import { Address } from '@elrondnetwork/erdjs/out';
+import { InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { denomination } from 'config';
+import { useOrganizationInfoContext } from 'pages/Organization/OrganizationInfoContextProvider';
 import { selectedTokenToSendSelector } from 'redux/selectors/modalsSelector';
 import { MultisigSendToken } from 'types/MultisigSendToken';
 
@@ -12,16 +16,29 @@ interface ProposeSendTokenType {
 const ProposeSendToken = ({ handleChange }: ProposeSendTokenType) => {
   const { t } = useTranslation();
 
-  const [address, setAddress] = useState('');
-  const [identifier, setIdentifier] = useState('');
-  const [amount, setAmount] = useState('');
   const selectedToken = useSelector(selectedTokenToSendSelector);
+  const [address, setAddress] = useState('');
+  const [identifier, setIdentifier] = useState(selectedToken.identifier);
+  const [amount, setAmount] = useState('');
 
-  console.log({ selectedToken });
+  const { tokensState } = useOrganizationInfoContext();
+  const [availableTokensWithBalances] = useState(
+    tokensState.map((token) => ({
+      identifier: token.identifier,
+      balance: operations.denominate({
+        input: token?.balance?.amount as string,
+        denomination: parseInt(token?.balance?.decimals as string) as number,
+        decimals: parseInt(token?.balance?.decimals as string) as number,
+        showLastNonZeroDecimal: true,
+        addCommas: false
+      })
+    }))
+  );
 
   const getProposal = (): MultisigSendToken | null => {
     try {
-      const amountNumeric = Number(amount);
+      const nominatedAmount = operations.nominate(amount, denomination);
+      const amountNumeric = Number(nominatedAmount);
       if (isNaN(amountNumeric)) {
         return null;
       }
@@ -47,12 +64,12 @@ const ProposeSendToken = ({ handleChange }: ProposeSendTokenType) => {
     setAddress(event.target.value);
   };
 
-  const onIdentifierChanged = (event: any) => {
-    setIdentifier(event.target.value);
-  };
-
   const onAmountChanged = (event: any) => {
     setAmount(event.target.value);
+  };
+
+  const onIdentifierChanged = (event: SelectChangeEvent) => {
+    setIdentifier(event.target.value as string);
   };
 
   React.useEffect(() => {
@@ -71,16 +88,33 @@ const ProposeSendToken = ({ handleChange }: ProposeSendTokenType) => {
           onChange={onAddressChanged}
         />
       </div>
-      <div className='modal-control-container'>
-        <label>{t('Identifier')}: </label>
-        <input
-          type='text'
-          className='form-control'
-          value={selectedToken.identifier}
-          autoComplete='off'
+      <div className='modal-control-container mb-4'>
+        <InputLabel id='demo-simple-select-label'>Identifier</InputLabel>
+        <Select
+          value={identifier}
+          fullWidth
+          label='Identifier'
           onChange={onIdentifierChanged}
-        />
+          className='mb-2'
+        >
+          {availableTokensWithBalances.map((token, idx) => {
+            return (
+              <MenuItem key={idx} value={token.identifier}>
+                {token.identifier?.split('-')[0]}
+              </MenuItem>
+            );
+          })}
+        </Select>
+        <div>
+          Balance:{' '}
+          {
+            availableTokensWithBalances.find(
+              (token) => token.identifier === identifier
+            )?.balance
+          }
+        </div>
       </div>
+
       <div className='modal-control-container'>
         <label>{t('Amount')}: </label>
         <input
