b56f2fb57379ee235a51520f4b8a962ba93a1c9d
Rares Raulea <rares@graffino.com>
2022-04-27 15:28:12 +0300

    feat(transactions): define custom hook for transaction permissions

---
+++ src/pages/Transactions/useTransactionPermissions.tsx
@@ -0,0 +1,56 @@
+import { useGetAccountInfo } from '@elrondnetwork/dapp-core';
+import { Address } from '@elrondnetwork/erdjs/out';
+import { useOrganizationInfoContext } from 'pages/Organization/OrganizationInfoContextProvider';
+import { MultisigActionDetailed } from 'types/MultisigActionDetailed';
+
+export default function useTransactionPermissions() {
+  const {
+    quorumCountState: [quorumCount],
+    boardMembersState: [boardMembers],
+    userRole
+  } = useOrganizationInfoContext();
+  const isBoardMember = userRole === 2;
+
+  const canUnsign = (action: MultisigActionDetailed) => {
+    return isBoardMember && alreadySigned(action);
+  };
+
+  const canPerformAction = (action: MultisigActionDetailed) => {
+    return (
+      isBoardMember &&
+      alreadySigned(action) &&
+      action.signers.length >= quorumCount
+    );
+  };
+
+  const canSign = (action: MultisigActionDetailed) => {
+    return isBoardMember && !alreadySigned(action);
+  };
+
+  const canDiscardAction = (action: MultisigActionDetailed) => {
+    return isBoardMember && action.signers.length === 0;
+  };
+
+  const { address } = useGetAccountInfo();
+
+  const alreadySigned = (action: MultisigActionDetailed) => {
+    if (!address) {
+      return false;
+    }
+    const typedAddress = new Address(address);
+    for (const signerAddress of action.signers) {
+      if (signerAddress.hex() === typedAddress.hex()) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  return {
+    canUnsign,
+    canPerformAction,
+    canSign,
+    canDiscardAction
+  };
+}
