71c5260da6063f011be6271d061c5d5fb74870d1
BoicaSebi <sebastian@graffino.com>
2022-04-28 15:38:33 +0300

    feat(addSafe): added add safe drawer

---
+++ src/components/Layout/Navbar/DrawerAccounts/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+import Divider from '@mui/material/Divider';
+
+const DrawerAccounts = () => {
+  return (
+    <Box>
+      <Divider />
+    </Box>
+  );
+};
+
+export default DrawerAccounts;
--- src/components/Layout/Navbar/NavbarAccountDetails/index.tsx
+++ src/components/Layout/Navbar/NavbarAccountDetails/index.tsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
-import CopyAllIcon from '@mui/icons-material/CopyAll';
 import NorthEastRoundedIcon from '@mui/icons-material/NorthEastRounded';
 import QrCode2Icon from '@mui/icons-material/QrCode2';
 import { Box, Button, Typography } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import { styled, useTheme } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
+import CopyButton from 'components/CopyButton';
+import DrawerAccounts from 'components/Layout/Navbar/DrawerAccounts';
 import ReceiveModal from 'components/ReceiveModal';
+import { uniqueContractAddress } from 'multisigConfig';
 import { currentMultisigContractSelector } from 'redux/selectors/multisigContractsSelectors';
 import { setProposeMultiselectSelectedOption } from 'redux/slices/modalsSlice';
 import { ProposalsTypes } from 'types/Proposals';
@@ -13,12 +21,26 @@ import { ProposalsTypes } from 'types/Proposals';
 const NavbarAccountDetails = ({ uniqueAddress, address }: any) => {
   const dispatch = useDispatch();
   const currentContract = useSelector(currentMultisigContractSelector);
+  const [open, setOpen] = React.useState(false);
+  const drawerWidth = 340;
 
   const [showQr, setShowQr] = useState(false);
+  const theme = useTheme();
 
   const handleQrModal = () => {
     setShowQr(!showQr);
   };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end'
+  }));
 
   const onAddBoardMember = () =>
     dispatch(
@@ -26,31 +48,99 @@ const NavbarAccountDetails = ({ uniqueAddress, address }: any) => {
         option: ProposalsTypes.multiselect_proposal_options
       })
     );
+  const addSafe = () =>
+    dispatch(
+      setProposeMultiselectSelectedOption({
+        option: ProposalsTypes.add_proposer
+      })
+    );
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
   return (
     <Box>
-      <Box
-        sx={{
-          width: '50px',
-          height: '50px',
-          backgroundColor: '#000',
-          margin: 'auto'
-        }}
-      ></Box>
-      <Box sx={{ pt: 1 }}>
-        <Typography align='center'>{uniqueAddress}</Typography>
+      <Box>
+        <Box
+          sx={{
+            width: '50px',
+            height: '50px',
+            backgroundColor: '#000',
+            margin: 'auto'
+          }}
+        ></Box>
+        <Box sx={{ pt: 1 }}>
+          <Typography align='center'>{uniqueAddress}</Typography>
+        </Box>
+        {!uniqueContractAddress && (
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{
+              mr: 2,
+              position: 'absolute',
+              top: '8px',
+              right: '0px',
+              ...(open && { display: 'none' })
+            }}
+          >
+            <ChevronRightIcon />
+          </IconButton>
+        )}
       </Box>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box'
+          }
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
+      >
+        <DrawerHeader>
+          <Box
+            sx={{ mt: 12, width: '100%' }}
+            className='d-flex align-items-center justify-content-between'
+          >
+            <Box className='d-flex'>
+              <Button className='align-items-start' onClick={addSafe}>
+                <AddCircleIcon />
+                <p>Add Safe</p>
+              </Button>
+            </Box>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </Box>
+        </DrawerHeader>
+        <DrawerAccounts />
+      </Drawer>
       <Box className='d-flex justify-content-center' sx={{ pt: 1 }}>
-        <Button onClick={handleQrModal}>
+        <Box onClick={handleQrModal} sx={{ mx: 1 }}>
           <QrCode2Icon />
-        </Button>
-        <CopyAllIcon />
-        <a
-          href={`https://devnet-explorer.elrond.com/accounts/${address}`}
-          target='_blank'
-          rel='noreferrer'
-        >
-          <ContentPasteSearchIcon />
-        </a>
+        </Box>
+        <Box className={'copy-btn'} sx={{ mx: 1 }}>
+          <CopyButton text={address} />
+        </Box>
+        <Box sx={{ mx: 1 }}>
+          <a
+            href={`https://devnet-explorer.elrond.com/accounts/${address}`}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <ContentPasteSearchIcon />
+          </a>
+        </Box>
         <ReceiveModal
           showQrFromSidebar={showQr}
           address={currentContract?.address}
