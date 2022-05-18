0ceaee5fe2a567fc2f88a50dd2046004c492285b
BoicaSebi <sebastian@graffino.com>
2022-05-18 10:43:43 +0300

    feat(totalBalance): added functionlaity in totalBalance component

--- src/components/Layout/Navbar/NavbarAccountDetails/index.tsx
+++ src/components/Layout/Navbar/NavbarAccountDetails/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
 import QrCode2Icon from '@mui/icons-material/QrCode2';
 import { Box, Button, Typography } from '@mui/material';
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import Safe from 'assets/img/safe.png';
 import CopyButton from 'components/CopyButton';
@@ -10,21 +9,10 @@ import ReceiveModal from 'components/ReceiveModal';
 import SafeOptions from 'components/SafeOptions';
 import { uniqueContractAddress } from 'multisigConfig';
 import { currentMultisigContractSelector } from 'redux/selectors/multisigContractsSelectors';
-import { setProposeMultiselectSelectedOption } from 'redux/slices/modalsSlice';
-import { ProposalsTypes } from 'types/Proposals';
 import './navbarAccountDetails.scss';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import { getNetworkProxy } from '@elrondnetwork/dapp-core';
-import { Address } from '@elrondnetwork/erdjs/out';
-import { network } from 'config';
-import {
-  setMultisigBalance,
-  setOrganizationTokens
-} from 'redux/slices/accountSlice';
 import { useOrganizationInfoContext } from 'pages/Organization/OrganizationInfoContextProvider';
-import { TokenWithPrice } from 'pages/Organization/types';
-import { priceSelector } from 'redux/selectors/economicsSelector';
 import TotalBalance from '../TotalBalance';
 
 const NavbarAccountDetails = ({ uniqueAddress }: { uniqueAddress: string }) => {
@@ -32,111 +20,16 @@ const NavbarAccountDetails = ({ uniqueAddress }: { uniqueAddress: string }) => {
   const currentContract = useSelector(currentMultisigContractSelector);
   const [showQr, setShowQr] = useState(false);
 
-  const egldPrice = useSelector(priceSelector);
-
-  const proxy = getNetworkProxy();
-  const getTokenPrice = useCallback(
-    (tokenIdentifier: string) =>
-      tokenPrices.find((tokenWithPrice: TokenWithPrice) => {
-        return tokenWithPrice.symbol == tokenIdentifier;
-      })?.price ?? egldPrice,
-    []
-  );
   const {
     tokenPrices,
     membersCountState: [membersCount]
   } = useOrganizationInfoContext();
-
-  const fetchTokenPhotoUrl = useCallback(async (tokenIdentifier: string) => {
-    const { data } = await axios.get(
-      `${network.apiAddress}/tokens/${tokenIdentifier}`
-    );
-
-    return data.assets.pngUrl;
-  }, []);
-
-  useEffect(() => {
-    (async function getTokens() {
-      let isMounted = true;
-
-      if (!currentContract?.address)
-        return () => {
-          isMounted = false;
-        };
-
-      const getEgldBalancePromise = currentContract?.address
-        ? proxy.getAccount(new Address(currentContract?.address))
-        : {};
-
-      const getAllOtherTokensPromise = axios.get(
-        `${network.apiAddress}/accounts/${currentContract?.address}/tokens`
-      );
-
-      try {
-        const [{ balance: egldBalance }, { data: otherTokens }] =
-          await Promise.all([getEgldBalancePromise, getAllOtherTokensPromise]);
-
-        if (!isMounted) return;
-
-        dispatch(setMultisigBalance(JSON.stringify(egldBalance)));
-
-        const allTokens = [
-          { ...egldBalance.token, balance: egldBalance.value.toString() },
-          ...otherTokens
-        ];
-
-        const tokensWithPrices = [];
-
-        for (const [idx, token] of Object.entries(allTokens)) {
-          const priceOfCurrentToken = getTokenPrice(token.identifier ?? '');
-
-          const { owner, ...tokenWithoutOwner } = token;
-
-          let photoUrl = '';
-
-          if (token.identifier !== 'EGLD')
-            photoUrl = await fetchTokenPhotoUrl(token.identifier as string);
-
-          tokensWithPrices.push({
-            ...tokenWithoutOwner,
-            presentation: {
-              tokenIdentifier: token.identifier,
-              photoUrl
-            },
-            id: idx,
-            balanceDetails: {
-              photoUrl,
-              identifier: token.identifier?.split('-')[0] ?? '',
-              amount: token.balance as string,
-              decimals: token.decimals as number
-            },
-            value: {
-              tokenPrice: priceOfCurrentToken,
-              decimals: token.decimals as number,
-              amount: token.balance as string
-            }
-          });
-        }
-
-        dispatch(setOrganizationTokens(tokensWithPrices));
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, [currentContract]);
 
   const [openedSafeSelect, setOpenedSafeSelect] = useState(false);
 
   const handleQrModal = () => {
     setShowQr(!showQr);
   };
-
-  const onAddBoardMember = () =>
-    dispatch(
-      setProposeMultiselectSelectedOption({
-        option: ProposalsTypes.multiselect_proposal_options
-      })
-    );
 
   const closeSafeDropdown = (data: boolean) => {
     setOpenedSafeSelect(data);
@@ -210,15 +103,6 @@ const NavbarAccountDetails = ({ uniqueAddress }: { uniqueAddress: string }) => {
         </Box>
       </Box>
       <TotalBalance />
-      <Box className='d-flex justify-content-center' sx={{ pb: 1 }}>
-        <Button
-          className='new-transfer-btn'
-          variant='outlined'
-          onClick={onAddBoardMember}
-        >
-          New Transaction
-        </Button>
-      </Box>
     </Box>
   );
 };
--- src/components/Layout/Navbar/TotalBalance/index.tsx
+++ src/components/Layout/Navbar/TotalBalance/index.tsx
@@ -1,19 +1,133 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { getNetworkProxy } from '@elrondnetwork/dapp-core';
 import { operations, Ui } from '@elrondnetwork/dapp-utils';
+import { Address } from '@elrondnetwork/erdjs/out';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import ChangeCurrency from 'components/ChangeCurrency';
+import { network } from 'config';
+import { useOrganizationInfoContext } from 'pages/Organization/OrganizationInfoContextProvider';
+import { TokenWithPrice } from 'pages/Organization/types';
 import { organizationTokensSelector } from 'redux/selectors/accountSelector';
+import { priceSelector } from 'redux/selectors/economicsSelector';
+import { currentMultisigContractSelector } from 'redux/selectors/multisigContractsSelectors';
+import {
+  setMultisigBalance,
+  setOrganizationTokens
+} from 'redux/slices/accountSlice';
+import { setProposeMultiselectSelectedOption } from 'redux/slices/modalsSlice';
+import { ProposalsTypes } from 'types/Proposals';
 
 const TotalBalance = () => {
+  const dispatch = useDispatch();
+
   const [totalUsdValue, setTotalUsdValue] = useState(0);
 
   const organizationTokens = useSelector(organizationTokensSelector);
+  const egldPrice = useSelector(priceSelector);
 
   const [openedCurencySelect, setOpenedCurencySelect] = useState(false);
   const [selectedCurrency, setSelectedCurrency] = useState('USD');
+
+  const onAddBoardMember = () =>
+    dispatch(
+      setProposeMultiselectSelectedOption({
+        option: ProposalsTypes.multiselect_proposal_options
+      })
+    );
+  const currentContract = useSelector(currentMultisigContractSelector);
+  const {
+    tokenPrices,
+    membersCountState: [membersCount]
+  } = useOrganizationInfoContext();
+  const proxy = getNetworkProxy();
+  const getTokenPrice = useCallback(
+    (tokenIdentifier: string) =>
+      tokenPrices.find((tokenWithPrice: TokenWithPrice) => {
+        return tokenWithPrice.symbol == tokenIdentifier;
+      })?.price ?? egldPrice,
+    []
+  );
+  const fetchTokenPhotoUrl = useCallback(async (tokenIdentifier: string) => {
+    const { data } = await axios.get(
+      `${network.apiAddress}/tokens/${tokenIdentifier}`
+    );
+
+    return data.assets.pngUrl;
+  }, []);
+
+  useEffect(() => {
+    (async function getTokens() {
+      let isMounted = true;
+
+      if (!currentContract?.address)
+        return () => {
+          isMounted = false;
+        };
+
+      const getEgldBalancePromise = currentContract?.address
+        ? proxy.getAccount(new Address(currentContract?.address))
+        : {};
+
+      const getAllOtherTokensPromise = axios.get(
+        `${network.apiAddress}/accounts/${currentContract?.address}/tokens`
+      );
+
+      try {
+        const [{ balance: egldBalance }, { data: otherTokens }] =
+          await Promise.all([getEgldBalancePromise, getAllOtherTokensPromise]);
+
+        if (!isMounted) return;
+
+        dispatch(setMultisigBalance(JSON.stringify(egldBalance)));
+
+        const allTokens = [
+          { ...egldBalance.token, balance: egldBalance.value.toString() },
+          ...otherTokens
+        ];
+
+        const tokensWithPrices = [];
+
+        for (const [idx, token] of Object.entries(allTokens)) {
+          const priceOfCurrentToken = getTokenPrice(token.identifier ?? '');
+
+          const { owner, ...tokenWithoutOwner } = token;
+
+          let photoUrl = '';
+
+          if (token.identifier !== 'EGLD')
+            photoUrl = await fetchTokenPhotoUrl(token.identifier as string);
+
+          tokensWithPrices.push({
+            ...tokenWithoutOwner,
+            presentation: {
+              tokenIdentifier: token.identifier,
+              photoUrl
+            },
+            id: idx,
+            balanceDetails: {
+              photoUrl,
+              identifier: token.identifier?.split('-')[0] ?? '',
+              amount: token.balance as string,
+              decimals: token.decimals as number
+            },
+            value: {
+              tokenPrice: priceOfCurrentToken,
+              decimals: token.decimals as number,
+              amount: token.balance as string
+            }
+          });
+        }
+
+        dispatch(setOrganizationTokens(tokensWithPrices));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [currentContract]);
 
   const totalValue = () => {
     const arrayOfUsdValues: Array<number> = [];
@@ -91,6 +205,15 @@ const TotalBalance = () => {
             />
           </Box>
         )}
+      </Box>
+      <Box className='d-flex justify-content-center' sx={{ pb: 1 }}>
+        <Button
+          className='new-transfer-btn'
+          variant='outlined'
+          onClick={onAddBoardMember}
+        >
+          New Transaction
+        </Button>
       </Box>
     </Box>
   );
