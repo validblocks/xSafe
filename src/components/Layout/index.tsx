f44028110cabddfc2ec410cf94108debae3ed217
BoicaSebi <sebastian@graffino.com>
2022-06-08 11:39:11 +0300

    style(safe): added safe deploy wallet and style

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -102,7 +102,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       duration: theme.transitions.duration.leavingScreen
     }),
     boxShadow: 'unset',
-    borderBottom: '1px solid #e0e0e0',
     right: 'auto',
     left: 'auto'
   }));
