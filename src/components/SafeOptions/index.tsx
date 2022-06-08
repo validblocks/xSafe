2165bd2c87bcd74afeba1f5bd04755cbb8ac8867
BoicaSebi <sebastian@graffino.com>
2022-06-08 11:39:11 +0300

    style(safe): added safe deploy wallet and style

--- src/components/SafeOptions/index.tsx
+++ src/components/SafeOptions/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AddIcon from '@mui/icons-material/Add';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import Divider from '@mui/material/Divider';
 import { useDispatch, useSelector } from 'react-redux';
@@ -21,9 +21,15 @@ import {
   InactiveWallet,
   SafeOptionsWrapper
 } from './safe-style';
+import { useNavigate } from 'react-router-dom';
+import { uniqueContractAddress } from 'multisigConfig';
+import DeployStepsModal from 'pages/Dashboard/DeployMultisigModal';
+import { setMultisigContracts } from 'redux/slices/multisigContractsSlice';
+import { MultisigContractInfoType } from 'types/multisigContracts';
 
 const SafeOptions = ({ closeSafeDropdown }: any) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [shortAddress, setShortAddress] = useState('');
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -38,9 +44,22 @@ const SafeOptions = ({ closeSafeDropdown }: any) => {
       })
     );
 
+  const [showDeployMultisigModal, setShowDeployMultisigModal] = useState(false);
+
+  async function onDeployClicked() {
+    setShowDeployMultisigModal(true);
+  }
+
   const currencyConverted = useSelector(currencyConvertedSelector);
   const getCurrency = useSelector(selectedCurrencySelector);
-
+  const onEnterClicked = () => {
+    navigate('/multisig/' + uniqueContractAddress);
+  };
+  async function updateMultisigContract(
+    newContracts: MultisigContractInfoType[]
+  ) {
+    dispatch(setMultisigContracts(newContracts));
+  }
   return (
     <SafeOptionsWrapper
     // onBlur={() => {
@@ -52,39 +71,57 @@ const SafeOptions = ({ closeSafeDropdown }: any) => {
       </Typography>
       <Divider />
       <AddSafeWrapper sx={{ p: 2, pl: 0 }}>
-        <AddSafe onClick={addSafe}>
+        <AddSafe onClick={onDeployClicked}>
           <AddIcon sx={{ mr: 1 }} />
           Add a new safe
         </AddSafe>
       </AddSafeWrapper>
       <Divider />
-      <Button sx={{ p: 0 }}>
-        <Box sx={{ p: 1 }} className='d-flex align-items-center'>
-          <img src={Safe} width='60px' height='60px' />
-          <ActiveWallet sx={{ ml: 2 }}>
-            <TypographyBold align='left'>My Safe</TypographyBold>
-            <Typography align='left'>{shortAddress}</Typography>
-            <TypographyBold align='left'>
-              ≈{currencyConverted.toFixed(2)}
-              {getCurrency}
-            </TypographyBold>
-          </ActiveWallet>
-          <Box>
-            <Checkbox {...label} disabled checked />
-          </Box>
+      <Button sx={{ p: 0, width: '100%' }} onClick={onEnterClicked}>
+        <Box sx={{ p: 1, width: '100%' }} className='d-flex align-items-center'>
+          <Grid sm={3}>
+            <img src={Safe} width='60px' height='60px' />
+          </Grid>
+          <Grid sm={7}>
+            <ActiveWallet sx={{ ml: 2 }}>
+              <TypographyBold align='left'>My Safe</TypographyBold>
+              <Typography align='left'>{shortAddress}</Typography>
+              <TypographyBold align='left'>
+                ≈{currencyConverted.toFixed(2)}
+                {getCurrency}
+              </TypographyBold>
+            </ActiveWallet>
+          </Grid>
+          <Grid sm={2}>
+            <Box>
+              <Checkbox {...label} disabled checked />
+            </Box>
+          </Grid>
         </Box>
       </Button>
       <Divider />
-      <Button sx={{ p: 0 }}>
-        <Box sx={{ p: 1 }} className='d-flex align-items-center'>
-          <img src={OtherSafe} width='60px' height='60px' />
-          <InactiveWallet sx={{ ml: 2 }}>
-            <TypographyBold align='left'>My Other Safe</TypographyBold>
-            <Typography align='left'>{shortAddress}</Typography>
-            <TypographyBold align='left'>14,590 USD</TypographyBold>
-          </InactiveWallet>
+      <Button sx={{ p: 0, width: '100%' }}>
+        <Box sx={{ p: 1, width: '100%' }} className='d-flex align-items-center'>
+          <Grid sm={3}>
+            <img src={OtherSafe} width='60px' height='60px' />
+          </Grid>
+          <Grid sm={7}>
+            <InactiveWallet sx={{ ml: 2 }}>
+              <TypographyBold align='left'>My Other Safe</TypographyBold>
+              <Typography align='left'>{shortAddress}</Typography>
+              <TypographyBold align='left'>14,590 USD</TypographyBold>
+            </InactiveWallet>
+          </Grid>
+          <Grid sm={2}>
+            <></>
+          </Grid>
         </Box>
       </Button>
+      <DeployStepsModal
+        show={showDeployMultisigModal}
+        handleClose={() => setShowDeployMultisigModal(false)}
+        setNewContracts={updateMultisigContract}
+      />
     </SafeOptionsWrapper>
   );
 };
