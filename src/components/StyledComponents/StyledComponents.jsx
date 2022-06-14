49f232d802903355d5d31ad42ab481ea3d75ac5f
BoicaSebi <sebastian@graffino.com>
2022-06-14 12:03:16 +0300

    style(mainButton): added main button style

--- src/components/StyledComponents/StyledComponents.jsx
+++ src/components/StyledComponents/StyledComponents.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export const MainButton = styled(Button)`
 <<<<<<< HEAD
+<<<<<<< HEAD
   color: var(--primary-color);
   border: 1px solid var(--primary-color);
   background-color: var(--primary-background);
@@ -12,6 +13,10 @@ export const MainButton = styled(Button)`
 =======
   color: var(--text-color);
   border: 1px solid var(--text-color);
+=======
+  color: var(--primary-color);
+  border: 1px solid var(--primary-color);
+>>>>>>> 8e9802d (style(mainButton): added main button style)
   background-color: var(--button-background);
   box-shadow: 0px 0px 8px var(--button-shadow);
 >>>>>>> ea6a7ec (style(nftComponent): added styled component and scss variables)
