80b1173a19aef1c0d40c4f00a3fb6f0d7cfa0ae5
BoicaSebi <sebastian@graffino.com>
2022-06-14 12:03:38 +0300

    style(mainButton): added main button style

--- src/components/StyledComponents/StyledComponents.jsx
+++ src/components/StyledComponents/StyledComponents.jsx
@@ -2,22 +2,6 @@ import { Button } from '@mui/material';
 import styled from 'styled-components';
 
 export const MainButton = styled(Button)`
-<<<<<<< HEAD
-<<<<<<< HEAD
-  color: var(--primary-color);
-  border: 1px solid var(--primary-color);
-  background-color: var(--primary-background);
-  box-shadow: 0px 0px 8px var(--primary-shadow);
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-=======
-  color: var(--text-color);
-  border: 1px solid var(--text-color);
-=======
-  color: var(--primary-color);
-  border: 1px solid var(--primary-color);
->>>>>>> 8e9802d (style(mainButton): added main button style)
-  background-color: var(--button-background);
-  box-shadow: 0px 0px 8px var(--button-shadow);
->>>>>>> ea6a7ec (style(nftComponent): added styled component and scss variables)
 `;
