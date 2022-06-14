ea8f3aeec67dd16ab8541b50463e82d2d59ead06
BoicaSebi <sebastian@graffino.com>
2022-06-14 12:01:56 +0300

    style(nftComponent): added styled component and scss variables

--- src/components/StyledComponents/StyledComponents.jsx
+++ src/components/StyledComponents/StyledComponents.jsx
@@ -2,10 +2,17 @@ import { Button } from '@mui/material';
 import styled from 'styled-components';
 
 export const MainButton = styled(Button)`
+<<<<<<< HEAD
   color: var(--primary-color);
   border: 1px solid var(--primary-color);
   background-color: var(--primary-background);
   box-shadow: 0px 0px 8px var(--primary-shadow);
   width: ${(props) => props.width};
   height: ${(props) => props.height};
+=======
+  color: var(--text-color);
+  border: 1px solid var(--text-color);
+  background-color: var(--button-background);
+  box-shadow: 0px 0px 8px var(--button-shadow);
+>>>>>>> ea6a7ec (style(nftComponent): added styled component and scss variables)
 `;
