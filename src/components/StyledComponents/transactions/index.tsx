6d9c1e627d3a0a821fcf538ffb7454f330fd9f24
Rares Raulea <rares@graffino.com>
2022-07-08 14:47:55 +0300

    feat(transactions): define styled components extending Accordion and AccordionSummary

--- src/components/StyledComponents/transactions/index.tsx
+++ src/components/StyledComponents/transactions/index.tsx
@@ -1,10 +1,18 @@
-import { Accordion } from '@mui/material';
+import { Accordion, AccordionSummary } from '@mui/material';
 import styled from 'styled-components';
 
 export const TransactionAccordion = styled(Accordion)`
   margin: 10px 0;
   border: none !important;
   outline: none !important;
-  box-shadow: 0px 14px 24px 0px #4c2ffc08;
-  border-radius: 10px;
+  box-shadow: 0px 14px 24px 0px #4c2ffc08 !important;
+  border-radius: 10px !important;
+  &:before {
+    display: none;
+  };
+`;
+
+export const TransactionAccordionSummary = styled(AccordionSummary)`
+  border: none !important;
+  outline: none !important;
 `;
