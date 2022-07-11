99dcfc8cbf7f002d8cf3f0e1e51b4920c43c0320
Alexandru Mordasan <alexandru@graffino.com>
2022-07-11 17:49:11 +0300

    wip: eslint run

--- src/pages/MultisigDetails/MultisigDetailsPage.tsx
+++ src/pages/MultisigDetails/MultisigDetailsPage.tsx
@@ -372,17 +372,25 @@ function MultisigDetailsPage() {
                 <div className="user-role">
                   <p className="icon">
                     <FontAwesomeIcon icon={faUser} />
-                    Role: <span className="text">{t(userRoleAsString)}</span>
+                    Role:
+                    {' '}
+                    <span className="text">{t(userRoleAsString)}</span>
                   </p>
                 </div>
                 <div className="wallet-name position-relative">
-                  <h3 className="text-center mb-0">{multisigName} </h3>
+                  <h3 className="text-center mb-0">
+                    {multisigName}
+                    {' '}
+                  </h3>
                 </div>
                 {deployedAt != null && (
                   <div className="created d-flex">
                     <p className="time">
-                      <FontAwesomeIcon icon={faCalendarAlt} className="icon" />{' '}
-                      Created: <span className="text">{deployedAt}</span>
+                      <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
+                      {' '}
+                      Created:
+                      {' '}
+                      <span className="text">{deployedAt}</span>
                     </p>
                   </div>
                 )}
@@ -417,7 +425,8 @@ function MultisigDetailsPage() {
                     denomination,
                     decimals,
                     showLastNonZeroDecimal: true,
-                  })}{' '}
+                  })}
+                  {' '}
                   {egldLabel}
                 </h2>
                 <h5 className="ex-currency text-center">
@@ -430,7 +439,8 @@ function MultisigDetailsPage() {
                       addCommas: false,
                     })}
                     usd={egldPrice}
-                  />{' '}
+                  />
+                  {' '}
                   USD
                 </h5>
               </div>
