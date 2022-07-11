17ed71dd80072b4ebbe10a0ae8a5221796dc6a11
Alexandru Mordasan <alexandru@graffino.com>
2022-07-11 13:06:18 +0300

    fix: added styled component - action button + hover effect

--- src/components/Theme/StyledComponents.jsx
+++ src/components/Theme/StyledComponents.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Select } from '@mui/material';
+import { Button, Typography, Box, Select } from '@mui/material';
 import styled from 'styled-components';
 
 export const MainButton = styled(Button)`
@@ -8,6 +8,17 @@ export const MainButton = styled(Button)`
   background-color: ${(props) => props.theme.palette.background.main};
   box-shadow: 0px 0px 8px
     ${(props) => props.theme.shadows.main};
+  text-transform: capitalize;
+  font-weight: ${(props) => props.theme.font.weight.lg};
+  padding:
+    ${(props) => props.theme.padding.value.sm}
+    ${(props) => props.theme.padding.value.lg}
+    ${(props) => props.theme.padding.value.xs};
+`;
+
+export const AssetActionButton = styled(MainButton)`
+  padding: 1px ${(props) => props.theme.padding.value.lg} 0 0;
+  transform: scale(0);
 `;
 
 export const MainSelect = styled(Select)`
@@ -25,4 +36,11 @@ export const TypographyBold = styled(Typography)`
 
 export const Main = styled.main`
   background-color: ${(props) => props.theme.palette.background.default};
+`;
+
+export const AssetValue = styled(Box)`
+  font-size: 17px;
+  color: ${(props) => props.theme.palette.black.main};
+  padding: 0;
+  margin: 0;
 `;
