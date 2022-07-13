4914066da99b353632443e58151970e271c0b7e5
Rares Raulea <rares@graffino.com>
2022-07-13 13:07:02 +0300

    chore: fix

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="react-scripts" />
 declare module 'react-csv';
 declare module 'redux-persist/es/integration/react';
+declare module 'redux-persist/integration/react';
 declare module 'redux-persist/es/persistReducer';
 declare module 'redux-persist/es/persistStore';
 declare module 'redux-persist/es/storage';
