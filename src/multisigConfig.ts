7dabd99b7f487e99fb34786fedd02cabc0672e27
Alexandru Mordasan <alexandru@graffino.com>
2022-07-14 11:51:38 +0300

    chore: cleanup and minor fixes

--- src/multisigConfig.ts
+++ src/multisigConfig.ts
@@ -1,3 +1,7 @@
 export const uniqueContractAddress =
+<<<<<<< HEAD
   'erd1qqqqqqqqqqqqqpgqp7kvx57gwty22z5zlanqgexga45yjhjectas25mxqa';
+=======
+  'erd1qqqqqqqqqqqqqpgq2z3n985ky5zhpa9ec40ksjfxvkgl2p80hdeq4585ga';
+>>>>>>> 29c0074 (chore: cleanup and minor fixes)
 export const uniqueContractName = 'Graffino';
