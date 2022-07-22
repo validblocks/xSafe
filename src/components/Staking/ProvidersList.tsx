6627f1e6112690bec8962aad3b3233bb4fda96aa
Rares Raulea <rares@graffino.com>
2022-07-22 14:50:17 +0300

    feat(stake): cleanup after custom hook use

--- src/components/Staking/ProvidersList.tsx
+++ src/components/Staking/ProvidersList.tsx
@@ -1,4 +1,3 @@
-import { operations } from '@elrondnetwork/dapp-utils';
 import {
   DataGrid,
   GridRenderCellParams,
@@ -6,132 +5,30 @@ import {
   GridSelectionModel,
   GRID_CHECKBOX_SELECTION_COL_DEF,
 } from '@mui/x-data-grid';
-import axios from 'axios';
 import { useCallback, useMemo, useState } from 'react';
-import { useQuery } from 'react-query';
-import { USE_QUERY_DEFAULT_CONFIG } from 'src/react-query/config';
-import { QueryKeys } from 'src/react-query/queryKeys';
-import { IAPRColumn, IFilledColumn, IProvider, IProviderColumn, IProviderIdentity } from 'src/types/staking';
-import { denomination, decimals } from 'src/config';
 import { Box } from '@mui/material';
-import pipe from 'src/utils/compose';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedStakingProvider } from 'src/redux/slices/modalsSlice';
 import { selectedStakingProviderSelector } from 'src/redux/selectors/modalsSelector';
+import useProviderIdentitiesAfterSelection from 'src/utils/useProviderIdentitiesAfterSelection';
+import { IAPRColumn, IFilledColumn, IProviderColumn } from 'src/types/staking';
 import ErrorOnFetchIndicator from '../Utils/ErrorOnFetchIndicator';
 import LoadingDataIndicator from '../Utils/LoadingDataIndicator';
 import APRColumn from './APRColumn';
 import ProviderColumn from './ProviderColumn';
 import FilledColumn from './FilledColumn';
 
-interface IdentityWithColumns extends IProviderIdentity {
-    id: string;
-    providerColumn: IProviderColumn;
-    aprColumn: IAPRColumn;
-    filledColumn: IFilledColumn;
-    numNodes: number;
-}
-
 interface Props {
     searchParam?: string;
 }
 
 const ProvidersList = ({ searchParam }: Props) => {
-  const fetchProviders = (): Promise<IProvider[]> =>
-    axios.get('https://api.elrond.com/providers').then((res) => res.data);
-
   const {
-    data: fetchedProviders,
-  } = useQuery(
-    [QueryKeys.FETCHED_PROVIDERS],
-    fetchProviders,
-    {
-      ...USE_QUERY_DEFAULT_CONFIG,
-      keepPreviousData: true,
-      refetchOnMount: false,
-    },
-  );
-
-  const buildColumns = useCallback(
-    (data: IProviderIdentity[]): IdentityWithColumns[] => data.map((provider: IProviderIdentity) => {
-      const stakedAmount = Number(operations.denominate({
-        input: provider.locked,
-        denomination,
-        decimals,
-        showLastNonZeroDecimal: true,
-      }).replaceAll(',', ''));
-
-      const providerBeforeIdentityFetch = fetchedProviders?.find((p) => p.identity === provider.identity);
-      const delegationCap = providerBeforeIdentityFetch?.delegationCap;
-
-      const denominatedDelegationCap = operations.denominate({
-        input: delegationCap ?? '1',
-        denomination,
-        decimals,
-        showLastNonZeroDecimal: true,
-      });
-
-      const delegationCapForCalculations = Number(denominatedDelegationCap.replaceAll(',', ''));
-
-      let filledPercentage = Number(Number((stakedAmount / delegationCapForCalculations) * 100).toFixed(1));
-      if (filledPercentage > 100) filledPercentage = 100;
-
-      return {
-        ...provider,
-        numNodes: providerBeforeIdentityFetch?.numNodes ?? 0,
-        id: provider.identity,
-        providerColumn: { avatar: provider.avatar, name: provider.name, website: provider.website },
-        aprColumn: {
-          apr: provider.apr ?? 0,
-        },
-        filledColumn: {
-          filledPercentage,
-        },
-      };
-    }), [fetchedProviders]);
-
-  const sortAfterNodes = useCallback((data: IdentityWithColumns[]) => data.sort((a, b) => b.numNodes - a.numNodes), []);
-
-  const bringValidBlocksFirst = useCallback((data: IdentityWithColumns[]) => {
-    const validBlocksIndex = data.findIndex((p) => p.identity === 'validblocks');
-    if (validBlocksIndex > -1) {
-      const validBlocks = data.splice(validBlocksIndex, 1);
-      data.unshift(validBlocks[0]);
-    }
-    return data;
-  }, []);
-
-  const filterBySearchParam = useCallback((data: IProviderIdentity[]) => {
-    if (!searchParam) return data;
-    return data.filter((p) => p.identity.toLowerCase().includes(searchParam.toLowerCase()));
-  }, [searchParam]);
-
-  const fetchProviderIdentities = () => {
-    const providerIds = fetchedProviders?.map((provider: IProvider) => provider.identity).join(',');
-    return axios.get(`https://api.elrond.com/identities?identities=${providerIds}`).then((res) => res.data);
-  };
-
-  const {
-    data: fetchedProviderIdentities,
-    isFetching: isFetchingProviderIdentities,
-    isLoading: isLoadingProviderIdentities,
-    isError: isErrorOnFetchingProviderIdentities,
-  } = useQuery(
-    [QueryKeys.FETCHED_PROVIDER_IDENTITIES],
-    fetchProviderIdentities,
-    {
-      ...USE_QUERY_DEFAULT_CONFIG,
-      keepPreviousData: true,
-      enabled: !!fetchedProviders,
-      refetchOnMount: false,
-      select: (data: IProviderIdentity[]) => pipe(
-        filterBySearchParam,
-        buildColumns,
-        sortAfterNodes,
-        bringValidBlocksFirst,
-      )(data),
-    },
-  );
+    fetchedProviderIdentities,
+    isFetchingProviderIdentities,
+    isLoadingProviderIdentities,
+    isErrorOnFetchingProviderIdentities,
+  } = useProviderIdentitiesAfterSelection({ searchParam });
 
   const columns = useMemo(
     () => {
