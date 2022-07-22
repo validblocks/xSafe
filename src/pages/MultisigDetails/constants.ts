188fd9501f896567edb630e822594110f8845d52
Rares Raulea <rares@graffino.com>
2022-07-22 12:51:12 +0300

    feat(modals): update modal titles

--- src/pages/MultisigDetails/constants.ts
+++ src/pages/MultisigDetails/constants.ts
@@ -2,15 +2,15 @@ import { ProposalsTypes } from 'src/types/Proposals';
 
 export const titles = {
   [ProposalsTypes.add_proposer]: 'add proposer',
-  [ProposalsTypes.send_egld]: 'send EGLD',
-  [ProposalsTypes.add_board_member]: 'add board Member',
-  [ProposalsTypes.remove_user]: 'remove user',
-  [ProposalsTypes.change_quorum]: 'change quorum',
-  [ProposalsTypes.issue_token]: 'issue token',
-  [ProposalsTypes.send_token]: 'send token',
-  [ProposalsTypes.send_nft]: 'send nft',
+  [ProposalsTypes.send_egld]: 'Send EGLD',
+  [ProposalsTypes.add_board_member]: 'Add board Member',
+  [ProposalsTypes.remove_user]: 'Remove user',
+  [ProposalsTypes.change_quorum]: 'Change quorum',
+  [ProposalsTypes.issue_token]: 'Issue token',
+  [ProposalsTypes.send_token]: 'Send token',
+  [ProposalsTypes.send_nft]: 'Send NFT',
   [ProposalsTypes.multiselect_proposal_options]: 'select proposal type',
-  [ProposalsTypes.smart_contract_call]: 'smart contract call',
+  [ProposalsTypes.smart_contract_call]: 'Smart contract call',
   [ProposalsTypes.deploy_contract]: 'Deploy smart contract',
   [ProposalsTypes.deploy_contract_from_source]:
     'Deploy smart contract from source',
@@ -21,4 +21,5 @@ export const titles = {
   [ProposalsTypes.attach_contract]: 'Attach contract',
   [ProposalsTypes.replace_owner]: 'Replace owner',
   [ProposalsTypes.edit_owner]: 'Edit owner',
+  [ProposalsTypes.stake_tokens]: 'Choose a Staking Provider',
 };
