3af9092f29a6816dd907d7b796127cb9587a4f3f
Rares Raulea <rares@graffino.com>
2022-07-22 12:51:52 +0300

    feat(utils): define reusable MultisStep form component

---
+++ src/components/Utils/MultistepForm.tsx
@@ -0,0 +1,65 @@
+import { Box } from '@mui/material';
+import React, { useCallback, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FinalStepActionButton, ChangeStepButton } from '../Theme/StyledComponents';
+
+interface IMultistepFormProps {
+    steps: React.ReactElement[];
+    finalActionHandler: () => void;
+    emitStepChange?: React.Dispatch<React.SetStateAction<number>>;
+    finalActionText?: string;
+}
+const MultistepForm = ({
+  steps,
+  finalActionText = 'Proceed',
+  finalActionHandler,
+  emitStepChange = () => null }: IMultistepFormProps) => {
+  const [activeStepNumber, setActiveStepNumber] = useState(1);
+  const [isNextButtonActive, setIsNextButtonActive] = useState(false);
+
+  const finalStep = steps.length;
+  const { t } = useTranslation();
+
+  const activeStepJSX = useMemo(() => React.cloneElement(steps[activeStepNumber - 1], {
+    stepNumber: activeStepNumber,
+    enableNextStep: setIsNextButtonActive,
+  }), [activeStepNumber, steps]);
+
+  const proceedToNextStep = useCallback(() => {
+    if (activeStepNumber < finalStep) {
+      setActiveStepNumber((activeStepNumber) => activeStepNumber + 1);
+      emitStepChange((activeStepNumber) => activeStepNumber + 1);
+    }
+  }, [activeStepNumber, emitStepChange, finalStep]);
+
+  const proceedToPreviousStep = useCallback(() => {
+    setActiveStepNumber((activeStepNumber) => activeStepNumber - 1);
+    emitStepChange((activeStepNumber) => activeStepNumber - 1);
+  }, [emitStepChange]);
+
+  return (
+    <Box>
+      <Box>
+        {activeStepJSX}
+      </Box>
+      <Box display={'flex'} gap={2} padding="2rem 3rem">
+        {activeStepNumber > 1 && (
+        <ChangeStepButton onClick={proceedToPreviousStep}>
+          {t('Back') as string}
+        </ChangeStepButton>
+        )}
+        {activeStepNumber < finalStep ? (
+          <ChangeStepButton disabled={!isNextButtonActive} onClick={proceedToNextStep}>
+            {t('Next') as string}
+          </ChangeStepButton>
+        ) : (
+          <FinalStepActionButton onClick={() => finalActionHandler()}>
+            {t(finalActionText) as string}
+          </FinalStepActionButton>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default MultistepForm;
