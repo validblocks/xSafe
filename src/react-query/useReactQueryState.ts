a9194d219a1a496fb34518a6d0e1efdf63765c9e
Rares Raulea <rares@graffino.com>
2022-07-22 14:51:38 +0300

    feat(utils): define hook for accessing react query state and getting item by identifier from a specfic state

---
+++ src/react-query/useReactQueryState.ts
@@ -0,0 +1,27 @@
+import { QueryClient } from 'react-query';
+import { QueryKeys } from './queryKeys';
+
+interface _UseReactQueryStateConfig {
+  stateIdentifier: QueryKeys;
+  itemIdentifierKey: string;
+  itemIdentifier: string;
+}
+
+export default function useReactQueryState(queryClient: QueryClient) {
+  const getStateByKey = (stateIdentifier: QueryKeys): unknown[] =>
+    queryClient.getQueryData(QueryKeys[stateIdentifier]) ?? [];
+
+  const getItemByKey = (
+    stateIdentifier: QueryKeys,
+    itemIdentifierKey: string,
+    itemIdentifier: string,
+  ) =>
+    getStateByKey(stateIdentifier).filter(
+      (item: any) => item[itemIdentifierKey] === itemIdentifier,
+    )[0];
+
+  return {
+    getStateByKey,
+    getItemByKey,
+  };
+}
