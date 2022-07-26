0e309b067a21f23180e85e40053be6d950f7037d
Alexandru Mordasan <alexandru@graffino.com>
2022-07-26 15:55:03 +0300

    updated Unlock's style & hover methods

--- src/pages/Unlock/index.tsx
+++ src/pages/Unlock/index.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DappUI, useGetLoginInfo } from '@elrondnetwork/dapp-core';
 import { faArrowRight, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { style } from '@mui/system';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
-import routeNames from 'src/routes/routeNames';
-import { ReactComponent as IconElrond } from 'src/assets/img/icon-elrond.svg';
-import { ReactComponent as IconLedger } from 'src/assets/img/icon-ledger.svg';
-import { ReactComponent as IconMaiar } from 'src/assets/img/icon-maiar.svg';
-import { network } from 'src/config';
-import { accessTokenServices, maiarIdApi } from 'src/services/accessTokenServices';
+import { ReactComponent as IconElrond } from 'assets/img/icon-elrond.svg';
+import { ReactComponent as IconLedger } from 'assets/img/icon-ledger.svg';
+import { ReactComponent as IconMaiar } from 'assets/img/icon-maiar.svg';
+import { network } from 'config';
+import { routeNames } from 'routes';
+import { accessTokenServices, maiarIdApi } from 'services/accessTokenServices';
 
 declare global {
   interface Window {
@@ -17,37 +18,34 @@ declare global {
   }
 }
 
-const UnlockTitleTootlip = (props: Record<string, unknown>) => (
-  <Tooltip id="connect-to-wallet-tooltip" {...props}>
-    Connect securely using one of the provided options
-  </Tooltip>
-);
+// const UnlockTitle = () => (
+//   <h5 className='unlock-title mb-spacer'>
+//     <OverlayTrigger
+//       placement='top'
+//       delay={{ show: 250, hide: 400 }}
+//       overlay={(props) => (
+//         <Tooltip id='connect-to-wallet-tooltip' {...props}>
+//           Connect securely using one of the provided options
+//         </Tooltip>
+//       )}
+//     >
+//       <a
+//         href='/#'
+//         onClick={(e) => {
+//           e.preventDefault();
+//         }}
+//         data-testid='infoConnect'
+//       >
+//         <FontAwesomeIcon
+//           icon={faInfoCircle}
+//           className='i-icon text-secondary'
+//         />
+//       </a>
+//     </OverlayTrigger>
+//   </h5>
+// );
 
-const UnlockTitle = () => (
-  <h5 className="unlock-title mb-spacer">
-    Connect to a wallet
-    <OverlayTrigger
-      placement="top"
-      delay={{ show: 250, hide: 400 }}
-      overlay={(props) => UnlockTitleTootlip(props)}
-    >
-      <a
-        href="/#"
-        onClick={(e) => {
-          e.preventDefault();
-        }}
-        data-testid="infoConnect"
-      >
-        <FontAwesomeIcon
-          icon={faInfoCircle}
-          className="i-icon text-secondary"
-        />
-      </a>
-    </OverlayTrigger>
-  </h5>
-);
-
-function Unlock() {
+const Unlock = () => {
   const [token, setToken] = useState('');
   const { loginMethod } = useGetLoginInfo();
 
@@ -59,112 +57,95 @@ function Unlock() {
       });
   }, []);
 
-  const [bgColor, setBgColor] = useState('');
-  const [textColor, setTextColor] = useState('');
-
-  const hoverOn = () => {
-    setBgColor('#4C2FFC');
-    setTextColor('#FFFF');
-  };
-
-  const hoverOff = () => {
-    setBgColor('#FFFF');
-    setTextColor('black');
-  };
-
   const loginParams = {
     callbackRoute: routeNames.dashboard,
     token,
     logoutRoute: routeNames.welcome,
-    buttonClassName: 'btn btn-unlock btn-block',
+    buttonClassName: 'btn btn-unlock btn-block'
   };
 
-  if (loginMethod !== '') {
+  if (loginMethod != '') {
     return <Navigate to={routeNames.dashboard} />;
   }
 
   return (
-    <div className="unlock-page m-auto">
-      <div className="card unlock text-center">
-        <UnlockTitle />
+    <div className='unlock-block'>
+      <div className='unlock text-center'>
         {!window.elrondWallet && (
           <a
-            rel="noreferrer"
-            href="https://chrome.google.com/webstore/detail/dngmlblcodfobpdpecaadgfbcggfjfnm?authuser=0&hl=en"
-            target="_blank"
-            className="btn btn-unlock btn-block"
-            style={{ backgroundColor: `${bgColor}` }}
-            onMouseEnter={() => hoverOn()}
-            onMouseLeave={() => hoverOff()}
+            rel='noreferrer'
+            href='https://chrome.google.com/webstore/detail/dngmlblcodfobpdpecaadgfbcggfjfnm?authuser=0&hl=en'
+            target='_blank'
+            className='btn btn-unlock btn-block'
           >
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="title" style={{ color: `${textColor}` }}>
-                Maiar DeFi Wallet
-              </div>
+            <div className='d-flex justify-content-between align-items-center'>
+              <div className='title'>Maiar DeFi Wallet</div>
             </div>
           </a>
         )}
 
         {window.elrondWallet && (
           <DappUI.ExtensionLoginButton {...loginParams}>
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="d-flex flex-row method">
+            <div className='d-flex justify-content-between align-items-center'>
+              <div className='d-flex flex-row method'>
                 <IconMaiar />
-                <div className="title">Maiar DeFi Wallet</div>
+                <div className='title'>Maiar DeFi Wallet</div>
               </div>
 
-              <FontAwesomeIcon icon={faArrowRight} className="arrow" />
+              <FontAwesomeIcon icon={faArrowRight} className='arrow' />
             </div>
           </DappUI.ExtensionLoginButton>
         )}
 
         <DappUI.WalletConnectLoginButton {...loginParams}>
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="d-flex flex-row method">
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className='d-flex flex-row method'>
               <IconMaiar />
-              <div className="title">Maiar App</div>
+              <div className='title'>Maiar App</div>
             </div>
 
-            <FontAwesomeIcon icon={faArrowRight} className="arrow" />
+            <FontAwesomeIcon icon={faArrowRight} className='arrow' />
           </div>
         </DappUI.WalletConnectLoginButton>
 
-        <DappUI.LedgerLoginButton loginButtonText="" {...loginParams}>
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="d-flex flex-row method">
+        <DappUI.LedgerLoginButton loginButtonText={''} {...loginParams}>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className='d-flex flex-row method'>
               <IconLedger />
-              <div className="title">Ledger</div>
+              <div className='title'>Ledger</div>
             </div>
 
-            <FontAwesomeIcon icon={faArrowRight} className="arrow" />
+            <FontAwesomeIcon icon={faArrowRight} className='arrow' />
           </div>
         </DappUI.LedgerLoginButton>
 
         <DappUI.WebWalletLoginButton {...loginParams}>
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="d-flex flex-row method">
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className='d-flex flex-row method'>
               <IconElrond />
-              <div className="title">Elrond Web Wallet</div>
+              <div className='title'>Elrond Web Wallet</div>
             </div>
-            <FontAwesomeIcon icon={faArrowRight} className="arrow" />
+            <FontAwesomeIcon icon={faArrowRight} className='arrow' />
           </div>
         </DappUI.WebWalletLoginButton>
+      </div>
 
-        <div className="mt-spacer">
-          <span className="text">New to Elrond?</span>
-        </div>
-        <div className="mt-1">
-          <a
-            className="link-style"
-            href={`${network.walletAddress}/create`}
-            {...{ target: '_blank' }}
-          >
-            Learn How to setup a wallet
-          </a>
-        </div>
+      <hr />
+
+      <div className='mt-3'>
+        <span>New to Elrond?</span>
+      </div>
+      <div className='mt-1'>
+        <a
+          className='link-style'
+          href={`${network.walletAddress}/create`}
+          {...{ target: '_blank' }}
+        >
+          Learn how to setup a wallet
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default Unlock;
