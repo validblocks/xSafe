97e1a340b25796876f37dbfd483dca58efb7c099
Alexandru Mordasan <alexandru@graffino.com>
2022-08-02 12:07:25 +0300

    remove padding top & bottom  Logo

--- src/components/Layout/Navbar/Logo/index.tsx
+++ src/components/Layout/Navbar/Logo/index.tsx
@@ -1,25 +1,27 @@
-import { dAppName } from 'src/config';
-import { uniqueContractAddress } from 'src/multisigConfig';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import routeNames from 'src/routes/routeNames';
+import { dAppName } from 'config';
+import { uniqueContractAddress } from 'multisigConfig';
+import { routeNames } from 'routes';
 import { NavLogo, Logo, DappName } from '../navbar-style';
 
 const NavbarLogo = () => {
-  const navigate = useNavigate();
   const handleRedirectToHome = () => {
     const route = uniqueContractAddress
-      ? `/multisig/${uniqueContractAddress}`
+      ? '/multisig/' + uniqueContractAddress
       : routeNames.welcome;
     navigate(route);
   };
 
+  const navigate = useNavigate();
+
   return (
     <NavLogo
       onClick={handleRedirectToHome}
-      className="d-flex align-items-center"
+      className='d-flex align-items-center'
     >
       <Logo />
-      <DappName className="d-flex align-items-center">{dAppName}</DappName>
+      <DappName className='d-flex align-items-center'>{dAppName}</DappName>
     </NavLogo>
   );
 };
