3448cb96a61d30c6d71fffa0191e2d8b3c15348d
Rares Raulea <rares@graffino.com>
2022-08-09 20:27:56 +0300

    feat(stake): add delegation function names as global constants

---
+++ src/types/staking/delegationFunctionNames.ts
@@ -0,0 +1,6 @@
+export enum delegationFunctionNames {
+  'delegate' = 'delegate',
+  'reDelegateRewards' = 'reDelegateRewards',
+  'claimRewards' = 'claimRewards',
+  'unDelegate' = 'unDelegate',
+}
