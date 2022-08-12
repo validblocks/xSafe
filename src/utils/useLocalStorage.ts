36a428e59e3beafe5e5286ca570b39f3eeef985d
Rares Raulea <rares@graffino.com>
2022-08-12 23:12:06 +0300

    feat(apps): create reactivity for local storage interaction using the observer pattern

--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { LOCAL_STORAGE_KEYS } from 'src/pages/Marketplace/localStorageKeys';
+import { LocalStorageObserver } from './LocalStorageObserver';
 
 export function useLocalStorage<T>(key: LOCAL_STORAGE_KEYS, initialValue: T) {
   const [storedValue, setStoredValue] = useState(() => {
@@ -14,11 +15,14 @@ export function useLocalStorage<T>(key: LOCAL_STORAGE_KEYS, initialValue: T) {
     }
   });
 
+  LocalStorageObserver.subscribe(key, setStoredValue);
+
   const setValue = (value: T) => {
     try {
       const valueToStore =
         value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
+      LocalStorageObserver.publish(key, valueToStore);
       if (typeof window !== 'undefined') {
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
       }
