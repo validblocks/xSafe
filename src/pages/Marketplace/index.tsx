1274458fec82bd7a6af67580ac3effe6acf59ae3
Rares Raulea <rares@graffino.com>
2022-08-12 14:35:17 +0300

    feat(apps): define marketplace page component

---
+++ src/pages/Marketplace/index.tsx
@@ -0,0 +1,33 @@
+import { Box } from '@mui/material';
+import { LOCAL_STORAGE_KEYS } from 'src/redux/slices/appConfigSlice';
+import { availableApps, MenuItem } from 'src/utils/menuItems';
+import { useLocalStorage } from 'src/utils/useLocalStorage';
+import AppCard from './AppCard';
+
+const Marketplace = () => {
+  const [installedApps, setInstalledApps, updateLocalStorage] = useLocalStorage(LOCAL_STORAGE_KEYS.INSTALLED_APPS, []);
+  return (
+    <Box display={'flex'} alignItems={'center'} gap={2}>
+      {
+        availableApps.map((app: MenuItem) => (
+          <AppCard
+            imgUrl={`https://picsum.photos/200/200?random=${Math.random()}`}
+            title={app.name}
+            description={app?.description}
+            actionButtonText={installedApps.includes(app.id) ? 'Installed' : 'Install'}
+            actionButtonOnClick={() => {
+              setInstalledApps((apps: string[]) => (
+                apps.includes(app.id)
+                  ? apps
+                  : [...apps, app.id]
+              ));
+              updateLocalStorage();
+            }}
+          />
+        ))
+        }
+    </Box>
+  );
+};
+
+export default Marketplace;
