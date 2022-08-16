abd48db23ba39a2b5525b7c97d0ccbadf7753aee
Alexandru Mordasan <alexandru@graffino.com>
2022-08-16 11:32:43 +0300

    restyle: identidier height after error

--- src/components/Assets/AssetsTable.tsx
+++ src/components/Assets/AssetsTable.tsx
@@ -200,6 +200,9 @@ const AssetsTable = ({ hasStakingActions = false }: Props) => {
               color: 'rgba(76, 47, 252, 0.54)',
             },
           },
+          'div.MuiDataGrid-cell-withRenderer': {
+            backgroundColor: 'red',
+          },
         }}
       />
       <ReceiveModal
