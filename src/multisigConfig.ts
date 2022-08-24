8519c2e592fd0a07e17fa1380b1db5553f087592
Rares Raulea <rares@graffino.com>
2022-08-24 18:15:35 +0300

    fix: update contract address

--- src/multisigConfig.ts
+++ src/multisigConfig.ts
@@ -1,4 +1,3 @@
 export const uniqueContractAddress =
-  'erd1qqqqqqqqqqqqqpgqp7kvx57gwty22z5zlanqgexga45yjhjectas25mxqa';
-// 'erd1qqqqqqqqqqqqqpgq5hfs4zxcvp7rgmwgcjvwg6m2zxpdugcvvcts8rj9zw';
+  'erd1qqqqqqqqqqqqqpgq5hfs4zxcvp7rgmwgcjvwg6m2zxpdugcvvcts8rj9zw';
 export const uniqueContractName = 'Graffino';
