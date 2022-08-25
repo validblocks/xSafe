8b48345623a433254d170ec26c537b98050cfba0
Alexandru Mordasan <alexandru@graffino.com>
2022-08-25 21:57:56 +0300

    fix: navbar style - menu

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -145,11 +145,6 @@ const MiniDrawer = () => {
                     expandIcon={<ArrowDropUpIcon />}
                     id="panel1a-header"
                     sx={{ pl: 0 }}
-                    className={
-                    locationString === el.link
-                      ? 'active link-decoration'
-                      : 'link-decoration'
-                    }
                   >
                     <ListItem
                       sx={{
