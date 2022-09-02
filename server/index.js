24ba478af34fa6b774a33ad88f3f0fa340775c86
Nick Ciolpan <nick@graffino.com>
2022-09-02 13:26:04 +0300

    feature(server): added cors

--- server/index.js
+++ server/index.js
@@ -1,41 +1,23 @@
 const express = require('express');
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const morgan = require('morgan');
+var cors = require('cors');
 const axios = require('axios');
-
-
-// /accounts/erd1qqqqqqqqqqqqqpgq5hfs4zxcvp7rgmwgcjvwg6m2zxpdugcvvcts8rj9zw/delegations?forceRefresh=true
 
 const app = express();
 
 // Configuration
 const PORT = 3000;
-const HOST = "localhost";
-
+const HOST = 'localhost';
+app.use(cors());
 app.use(morgan('dev'));
 
-app.get('/proxy', (req, res, next) => {
-	const { route } = req.query;
-	axios.get(route).then(response => {
-		res.send(response.data)
-	});
+app.get('/proxy', (req, res, _next) => {
+  const { route } = req.query;
+  axios.get(route).then((response) => {
+    res.send(response.data);
+  });
 });
 
-// app.use('', (req, res, next) => {
-// if (req.headers.authorization) {
-// 	next();
-// } else {
-// 	res.sendStatus(403);
-// }});
-
-// app.use('/json_placeholder', createProxyMiddleware({
-// 	target: API_SERVICE_URL,
-// 	changeOrigin: true,
-// 	pathRewrite: {
-// 		[`^/json_placeholder`]: '',
-// 	},
-// }));
-
 app.listen(PORT, HOST, () => {
-	console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
