e8e98c79fa03bd2f90e8e588a4427c25e7ed42be
Rares Raulea <rares@graffino.com>
2022-09-02 19:35:55 +0300

    feat(proposalData): add delegation proposal data in queue and history

--- src/components/Staking/FilledColumn.tsx
+++ src/components/Staking/FilledColumn.tsx
@@ -3,10 +3,10 @@ import ContrastRoundedIcon from '@mui/icons-material/ContrastRounded';
 import PercentageWithIcon from '../Utils/PercentageWithIcon';
 
 interface Props {
-    columnData: IFilledColumn
+    columnData?: IFilledColumn
 }
 
-const FilledColumn = ({ columnData: { filledPercentage } }: Props) => (
+const FilledColumn = ({ columnData: { filledPercentage } = { filledPercentage: 100 } }: Props) => (
   <PercentageWithIcon
     icon={<ContrastRoundedIcon sx={{ color: 'rgba(8, 4, 29, 0.54)' }} />}
     percentage={filledPercentage === Infinity ? 'Uncapped' : filledPercentage.toString()}
--- src/types/MultisigSmartContractCall.tsx
+++ src/types/MultisigSmartContractCall.tsx
@@ -10,6 +10,12 @@ import {
 import i18next from 'i18next';
 import startCase from 'lodash/startCase';
 import MultisigSmartContractCallPresentation from 'src/types/MultisigSmartContractCallPresentation';
+import { Box, Grid } from '@mui/material';
+import StakingProviderBasedOnAddress from 'src/components/Staking/StakingProviderBasedOnAddress';
+import TokenPresentationWithPrice from 'src/components/Utils/TokenPresentationWithPrice';
+import { Text } from 'src/components/StyledComponents/StyledComponents';
+import EastIcon from '@mui/icons-material/East';
+import WestIcon from '@mui/icons-material/West';
 import { MultisigAction } from './MultisigAction';
 import { MultisigActionType } from './MultisigActionType';
 import { multisigContractFunctionNames } from './multisigFunctionNames';
@@ -24,6 +30,8 @@ export class MultisigSmartContractCall extends MultisigAction {
 
   args: BytesValue[];
 
+  static stakingProviders: any[];
+
   constructor(
     address: Address,
     amount: BigUIntValue,
@@ -35,6 +43,7 @@ export class MultisigSmartContractCall extends MultisigAction {
     this.amount = amount;
     this.functionName = functionName;
     this.args = args;
+    MultisigSmartContractCall.stakingProviders = [];
   }
 
   tooltip() {
@@ -77,7 +86,7 @@ export class MultisigSmartContractCall extends MultisigAction {
       case delegationFunctionNames.claimRewards:
         return i18next.t('Claim Rewards');
       case delegationFunctionNames.withdraw:
-        return i18next.t('Withdraw Delegation');
+        return i18next.t('Withdraw Rewards');
       default:
         return 'Unknown function';
     }
@@ -91,6 +100,16 @@ export class MultisigSmartContractCall extends MultisigAction {
         return this.getSendTokenDescription();
       case multisigContractFunctionNames.ESDTNFTTransfer:
         return this.getSendNFTDescription();
+      case delegationFunctionNames.delegate:
+        return this.getStakeTokensDescription('Stake to', <EastIcon />);
+      case delegationFunctionNames.unDelegate:
+        return this.getStakeTokensDescription('Unstake from', <WestIcon />);
+      case delegationFunctionNames.withdraw:
+        return this.getStakeTokensDescription('Withdraw rewards', <WestIcon />);
+      case delegationFunctionNames.claimRewards:
+        return this.getStakeTokensDescription('Claim rewards', <WestIcon />);
+      case delegationFunctionNames.reDelegateRewards:
+        return this.getStakeTokensDescription('Restake rewards', <WestIcon />);
       default:
         return 'Unknown function';
     }
@@ -120,6 +139,44 @@ export class MultisigSmartContractCall extends MultisigAction {
 
     return (
       <MultisigSmartContractCallPresentation identifier={identifier} amount={amount} address={this.address} />
+    );
+  }
+
+  getStakeTokensDescription(actionMessage: string, actionIcon: any) {
+    return (
+      <Grid container display="flex" justifyContent="flex-start" gap={2}>
+        <Grid
+          item
+          border={'1px solid #DFDFE8'}
+          padding={'1rem'}
+          display={'flex'}
+          justifyContent={'flex-start'}
+          alignItems={'center'}
+          borderRadius={'10px'}
+        >
+          <Box
+            marginRight={2}
+            paddingRight={2}
+            borderRight={'1px solid #DFDFE8'}
+          >
+            <TokenPresentationWithPrice withTokenAmount={false} withTokenValue={false} identifier="EGLD" />
+          </Box>
+          <Text display={'flex'} flexDirection={'column'}>
+            <Text fontWeight={500} marginRight={1}> Amount: </Text>
+            <Text>
+              {/* {Balance.fromString(this.amount.valueOf().toString()).toDenominated()} $EGLD */}
+              {this.amount.valueOf().toString()} $EGLD
+            </Text>
+          </Text>
+        </Grid>
+        <Grid item display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+          {actionMessage}
+          {actionIcon}
+        </Grid>
+        <Grid item>
+          <StakingProviderBasedOnAddress providerAddress={this.address.bech32()} />
+        </Grid>
+      </Grid>
     );
   }
 
