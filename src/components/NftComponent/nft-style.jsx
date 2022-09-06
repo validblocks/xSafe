b244a32b59669772acf436efbea4feec434fb7bb
Alexandru Mordasan <alexandru@graffino.com>
2022-09-06 11:59:09 +0300

    restyle: nfts & modal

--- src/components/NftComponent/nft-style.jsx
+++ src/components/NftComponent/nft-style.jsx
@@ -1,3 +1,4 @@
+import { Divider } from '@mui/material';
 import Card from '@mui/material/Card';
 import styled from 'styled-components';
 
@@ -11,68 +12,19 @@ export const CollectionName = styled.div`
   width: 100%;
 `;
 
-export const TextDivider = styled.div`
-  margin-top: 0;
-  margin-bottom: 2rem;
-  position: relative;
-  & .collectionLight{
-    font-weight: 500;
-    font-size: 11px;
-    color: #7A7883; 
-  }
-  & div {
-    display: flex;
-    align-items: center;
-    span:nth-child(1){
-      font-size: 16px;
-    }
-    span:nth-child(2){
-      margin: 0 0 1px .4rem;
-      line-height: 1;
-    }
-    :after {
-      content: '';
-      width: 100%;
-      height: 1px;
-      margin: 1px 0 0 .4rem;
-      background-color: #DFDFE8;
-    }
+export const TextDivider = styled(Divider)`
+  &:before {
+    content: '';
+    width: 1%;
   }
 `;
 
 export const CardBox = styled(Card)`
   height: ${(props) => props.height};
-  width: 250px;
-  margin: 0 auto 1.6rem auto !important;
+  width: 270px;
+  padding: .25rem .3rem 0 .3rem;
   border-radius: .7rem;
-  box-shadow: none;
-  transition: all .2s linear;
-  transform: scale(1);
-  overflow: hidden;
-  & span {
-    position: absolute;
-    width: 100%;
-    font-size: 18px;
-    font-weight: 600;
-    text-align: center;
-    top: 35px;
-    left: 0;
-    transition: font-size .2s linear, top .2s linear;
-  };
-  & button {
-    position: absolute;
-    width: 225.63px;
-    bottom: -50px;
-    left: 11.2px;
-    transition: bottom .2s linear;
-  };
-  &:hover{
-    & span {
-      top: 13px;
-      font-size: 15px;
-    };
-    & button {
-      bottom: 15px;
-    };
-  };
+  box-shadow:
+    0px 2px 10px rgba(76, 47, 252, 0.03),
+    0px 3px 12px rgba(76, 47, 252, 0.03);
 `;
