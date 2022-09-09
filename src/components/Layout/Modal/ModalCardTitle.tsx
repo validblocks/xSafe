0a3eeac1f7615ca01c01bbd3fed06ac23b03c66c
Alexandru Mordasan <alexandru@graffino.com>
2022-09-09 16:17:16 +0300

    fix(restyle): modal card title - first letter uppercase

--- src/components/Layout/Modal/ModalCardTitle.tsx
+++ src/components/Layout/Modal/ModalCardTitle.tsx
@@ -11,7 +11,7 @@ interface IProps {
 const ModalCardTitle = ({ title, handleClose, activeStepNumber = 0, totalSteps = 0 }: IProps) => (
   <ModalCardTitleContainer sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 2 }}>
     <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-      <Text fontSize="24px"> {title}</Text>
+      <Text fontSize="24px" sx={{ '&:first-letter': { textTransform: 'uppercase !important' } }}> {title}</Text>
       <Typography sx={{ mt: '0.3rem' }}>
         {totalSteps > 0 && <Text color="black.main" fontSize="12px"> Step {activeStepNumber} of {totalSteps}</Text>}
       </Typography>
