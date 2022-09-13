178a750173f3404ba2de46ece7f2258f8bc38604
Rares Raulea <rares@graffino.com>
2022-09-13 15:43:44 +0300

    fix: retry deploy

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Configuration
-// const corsOptions = {
-//   origin: 'http://localhost:3001',
-//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
 
 const PORT = 3000;
 const HOST = 'localhost';
