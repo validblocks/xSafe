c970e8c188042898841d5b00d3f1d7e26191003f
Alexandru Mordasan <alexandru@graffino.com>
2022-10-31 17:39:09 +0200

    forgot to commit copyButton changes

--- src/components/CopyButton/index.tsx
+++ src/components/CopyButton/index.tsx
@@ -1,20 +1,46 @@
+import { MouseEvent, useState } from 'react';
 import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useTheme } from 'styled-components';
-import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
-import { MouseEvent, useState } from 'react';
+import { Link } from '@mui/material';
 import copyTextToClipboard from './helpers/copyToClipboard';
-import * as Styled from '../Utils/styled/index';
 import { ReactComponent as CopyIcon } from '../../assets/img/copy.svg';
+
+interface Props {
+  link: any;
+  handle: (e: MouseEvent) => Promise<void>;
+  className: string;
+  resultOfCopySucces: boolean;
+  resultOfCopyDefault: boolean;
+}
+
+const Wrapper = (props: Props) => {
+  const { link: Link, handle, className, resultOfCopyDefault, resultOfCopySucces } = props;
+  return (
+    <Link
+      href="/#"
+      onClick={handle}
+      className={`side-action ${className}`}
+    >
+      {resultOfCopyDefault || !resultOfCopySucces ? (
+        <CopyIcon />
+      ) : (
+        <FontAwesomeIcon icon={faCheck} className="text-primary-highlight" />
+      )}
+    </Link>
+  );
+};
 
 interface CopyButtonType {
   text: string;
   className?: string;
-  color?: 'grey' | 'purple' | string;
+  link: typeof Link;
 }
 
-const CopyButton = ({ text, color = '', className = '' }: CopyButtonType) => {
-  const theme: any = useTheme();
+const CopyButton = ({
+  text,
+  className = '',
+  link,
+}: CopyButtonType) => {
   const [copyResult, setCopyResut] = useState({
     default: true,
     success: false,
@@ -38,75 +64,14 @@ const CopyButton = ({ text, color = '', className = '' }: CopyButtonType) => {
     }, 1000);
   };
 
-  if (color === 'grey') {
-    return (
-      <Styled.CopyIconLink
-        href="/#"
-        onClick={handleCopyToClipboard}
-        className={`side-action ${className}`}
-      >
-        {copyResult.default || !copyResult.success ? (
-          <CopyIcon />
-        ) : (
-          <FontAwesomeIcon icon={faCheck} className="text-primary-highlight" />
-        )}
-      </Styled.CopyIconLink>
-    );
-  }
-  if (color === 'purple') {
-    return (
-      <Styled.CopyIconLinkPurple
-        href="/#"
-        onClick={handleCopyToClipboard}
-        className={`side-action ${className}`}
-      >
-        {copyResult.default || !copyResult.success ? (
-          <CopyIcon />
-        ) : (
-          <FontAwesomeIcon icon={faCheck} className="text-primary-highlight" />
-        )}
-      </Styled.CopyIconLinkPurple>
-    );
-  }
-  if (color === 'connected-account') {
-    return (
-      <Styled.CopyIconLinkConnectedAccount
-        href="/#"
-        onClick={handleCopyToClipboard}
-        className={`side-action ${className}`}
-      >
-        {copyResult.default || !copyResult.success ? (
-          <CopyIcon />
-        ) : (
-          <FontAwesomeIcon icon={faCheck} className="text-primary-highlight" />
-        )}
-      </Styled.CopyIconLinkConnectedAccount>
-    );
-  }
-
   return (
-    <Styled.CopyIconLink
-      href="/#"
-      onClick={handleCopyToClipboard}
-      className={`side-action ${className}`}
-      sx={{
-        '& .text-secondary': {
-          color: '#6c757d !important',
-        },
-        '& .copyIcon': {
-          color: '#4c2ffc8a !important',
-        },
-        '& svg': {
-          color: `${theme.palette.button.copy} !important`,
-        },
-      }}
-    >
-      {copyResult.default || !copyResult.success ? (
-        <ContentCopyRoundedIcon />
-      ) : (
-        <FontAwesomeIcon icon={faCheck} className="text-primary-highlight" />
-      )}
-    </Styled.CopyIconLink>
+    <Wrapper
+      link={link}
+      handle={handleCopyToClipboard}
+      className={className}
+      resultOfCopyDefault={copyResult.default}
+      resultOfCopySucces={copyResult.success}
+    />
   );
 };
 
