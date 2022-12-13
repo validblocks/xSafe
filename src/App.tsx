58f7c6f20005ae1e18bce500d0049c73c37c94f9
Rares Raulea <rares@graffino.com>
2022-12-13 17:56:14 +0200

    feat(dappCoreUpdate): update 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,24 @@ import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import i18next from 'i18next';
 import { PersistGate } from 'redux-persist/integration/react';
 import routes from 'src/routes';
-import { DappProvider } from '@elrondnetwork/dapp-core/wrappers';
-import {
-  TransactionsToastList,
-  SignTransactionsModals,
-  NotificationModal,
-} from '@elrondnetwork/dapp-core/UI';
-import i18next from 'i18next';
 import { englishTranslations } from './i18n/en';
 import { germanTranslations } from './i18n/de';
 import Layout from './components/Layout';
 import PageNotFound from './components/PageNotFound';
+
+import '@elrondnetwork/dapp-core/build/index.css';
 import { persistor, store } from './redux/store';
 import OrganizationInfoContextProvider from './pages/Organization/OrganizationInfoContextProvider';
 import CustomThemeProvider from './components/Theme/CustomThemeProvider';
+import { DappProvider } from '@elrondnetwork/dapp-core/wrappers';
+import {
+  TransactionsToastList,
+  SignTransactionsModals,
+  NotificationModal
+} from '@elrondnetwork/dapp-core/UI';
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
@@ -74,9 +76,9 @@ export const App = () => (
           <QueryClientProvider client={queryClient}>
             <OrganizationInfoContextProvider>
               <>
+                <SignTransactionsModals />
                 <TransactionsToastList />
                 <NotificationModal />
-                <SignTransactionsModals className="custom-class-for-modals" />
                 <Router basename={process.env.PUBLIC_URL}>
                   <Layout>
                     <Routes>
