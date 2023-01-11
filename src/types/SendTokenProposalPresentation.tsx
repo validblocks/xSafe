64abccc3449651e86d928b59d3c4badf67f820c4
Rares Raulea <rares@graffino.com>
2023-01-11 19:34:30 +0200

    feat(transactions): improve send token and egld UI

--- src/types/SendTokenProposalPresentation.tsx
+++ src/types/SendTokenProposalPresentation.tsx
@@ -1,17 +1,11 @@
 import { Address, TokenPayment } from '@elrondnetwork/erdjs/out';
-import { Box, Typography, useMediaQuery } from '@mui/material';
-import { withStyles } from '@mui/styles';
-import i18next from 'i18next';
+import { Box, Grid, useMediaQuery } from '@mui/material';
+import SouthIcon from '@mui/icons-material/South';
 import MemberPresentationWithPhoto from 'src/pages/Organization/MemberPresentationWithPhoto';
 import BigNumber from '@elrondnetwork/erdjs/node_modules/bignumber.js';
 import TokenPresentationWithPrice from 'src/components/Utils/TokenPresentationWithPrice';
 import { Text } from 'src/components/StyledComponents/StyledComponents';
-
-const StyledTypography = withStyles({
-  root: {
-    letterSpacing: 0.5,
-  },
-})(Typography);
+import { StyledStakingProvider } from 'src/components/StyledComponents/staking';
 
 interface ISendTokenProposalPresentationProps {
   address: Address;
@@ -25,35 +19,45 @@ const SendTokenProposalPresentation = (
   const maxWidth600 = useMediaQuery('@media(max-width:600px)');
   const maxWidth500 = useMediaQuery('@media(max-width:500px)');
   return (
-    <Box>
+    <Box display="flex" flexDirection="column" alignItems="start">
       <Text style={{ fontSize: maxWidth600 ? 17 : '1.5rem' }}>
         <strong>{title}</strong>
       </Text>
-      <Box sx={{ py: '1rem' }}>
-        <TokenPresentationWithPrice identifier={identifier} withTokenAmount={false} withTokenValue={false} />
-      </Box>
-      <Box>
-        <StyledTypography
-          sx={{
-            color: 'rgb(93, 109, 116)',
-            letterSpacing: 0.5,
-            marginRight: '0.75rem',
-          }}
+      <StyledStakingProvider
+        sx={{
+          padding: '6px',
+          maxWidth: '200px',
+          my: 1,
+        }}
+      >
+        <Box
+          marginRight={2}
+          paddingRight={2}
+          borderRight={'1px solid #DFDFE8'}
         >
-          <strong className="mr-3">{i18next.t('Amount') as string}:</strong>
+          <TokenPresentationWithPrice withTokenAmount={false} withTokenValue={false} identifier={identifier} />
+        </Box>
+        <Box display={'flex'} flexDirection={'column'}>
+          <Text fontWeight={500} marginRight={1}> Amount: </Text>
           {Number(TokenPayment.egldFromBigInteger(amount).toRationalNumber()).toLocaleString() ?? '0'}{' '}
-        </StyledTypography>
-      </Box>
-      <Box sx={{ display: 'flex' }}>
-        <StyledTypography sx={{ marginRight: '0.75rem' }}>
-          <strong>To: </strong>
-        </StyledTypography>
+        </Box>
+      </StyledStakingProvider>
+      <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        <SouthIcon />
+        <Text mx={1}>Send to</Text>
+        <SouthIcon />
+      </Grid>
+      <StyledStakingProvider sx={{
+        padding: '6px',
+        my: 1,
+      }}
+      >
         <MemberPresentationWithPhoto
           memberAddress={address}
           // eslint-disable-next-line no-nested-ternary
           charactersLeftAfterTruncation={maxWidth500 ? 8 : maxWidth600 ? 16 : 20}
         />
-      </Box>
+      </StyledStakingProvider>
     </Box>
   );
 };
