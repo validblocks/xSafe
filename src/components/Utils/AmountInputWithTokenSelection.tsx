bdbd423b49516e997485f0877ed638fedb8e6d82
Rares Raulea <rares@graffino.com>
2023-01-12 19:37:28 +0200

    feat(transtions): add reusable amount input with token selection component

---
+++ src/components/Utils/AmountInputWithTokenSelection.tsx
@@ -0,0 +1,143 @@
+import { Box } from '@mui/material';
+import * as StyledRemote from 'src/pages/MultisigDetails/ProposeMultiselectModal/styled';
+import { useTranslation } from 'react-i18next';
+import { MaxSendEGLDButton } from 'src/components/Theme/StyledComponents';
+import { NumericFormat } from 'react-number-format';
+import { OrganizationToken } from 'src/pages/Organization/types';
+import { useCallback, useMemo } from 'react';
+import { accountSelector, getTokenPhotoById } from 'src/redux/selectors/accountSelector';
+import { createDeepEqualSelector } from 'src/redux/selectors/helpers';
+import { StateType } from '@elrondnetwork/dapp-core/reduxStore/slices';
+import { useSelector } from 'react-redux';
+import { selectedTokenToSendSelector } from 'src/redux/selectors/modalsSelector';
+import { FormikProps } from 'formik';
+import { useTheme } from 'styled-components';
+import { Text } from '../StyledComponents/StyledComponents';
+import TokenSelection from './TokenSelection';
+import TokenPresentationWithPrice from './TokenPresentationWithPrice';
+
+interface IProps {
+    handleMaxButtonClick?: () => any;
+    handleInputChange: any;
+    handleInputBlur: any;
+    resetAmount: any;
+    amount: string | number;
+    amountError: string | false | undefined;
+    formik?: FormikProps<any>;
+    config?: Partial<{
+        withAvailableAmount: boolean;
+        withTokenSelection: boolean;
+    }>
+}
+
+const AmountInputWithTokenSelection = ({
+  handleMaxButtonClick,
+  handleInputChange,
+  handleInputBlur,
+  amount,
+  amountError,
+  resetAmount,
+  formik,
+  config: {
+    withAvailableAmount,
+    withTokenSelection,
+  } = {
+    withAvailableAmount: true,
+    withTokenSelection: true,
+  },
+}: IProps) => {
+  const { t } = useTranslation();
+  const theme: any = useTheme();
+  const { identifier = 'EGLD' } = useSelector(selectedTokenToSendSelector);
+  const selector = useMemo(
+    () => createDeepEqualSelector(accountSelector, (state: StateType) => getTokenPhotoById(state, identifier)),
+    [identifier]);
+
+  const {
+    tokenAmount,
+    prettyIdentifier,
+  } = useSelector<StateType, OrganizationToken>(selector);
+
+  const maxButtonClickHandler = useCallback(() => {
+    if (handleMaxButtonClick) {
+      return handleMaxButtonClick();
+    }
+    return formik?.setFieldValue('amount', tokenAmount);
+  }, [formik, handleMaxButtonClick, tokenAmount]);
+
+  return (
+    <StyledRemote.AmountWithTokenSelectionBox
+      className={amountError != null ? 'invalid' : ''}
+      sx={{
+        display: 'flex !important', alignItems: 'center', justifyContent: 'space-between',
+      }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <NumericFormat
+          name="amount"
+          id="amount"
+          value={amount}
+          thousandSeparator
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          className={amountError != null ? 'isError' : ''}
+          style={{
+            width: '100%',
+            borderRadius: 10,
+            background: 'transparent',
+            border: 'none',
+            flex: '1',
+          }}
+        />
+
+        <label htmlFor={amount.toString()} className={amountError != null ? 'isError' : ''}>
+          {`${t('Amount')}`}
+        </label>
+      </Box>
+      <Box px={1}>
+        <MaxSendEGLDButton onClick={maxButtonClickHandler}>
+          Max
+        </MaxSendEGLDButton>
+      </Box>
+      {withTokenSelection ? (
+        <Box minWidth="160px">
+          <TokenSelection
+            amountError={amountError}
+            resetAmount={resetAmount}
+          />
+        </Box>
+      ) : (
+        <Box
+          className="egld-staked"
+          sx={{
+            borderLeft: `1px solid ${theme.palette.borders.secondary}`,
+            borderTopLeftRadius: '5rem',
+            borderBottomLeftRadius: '5rem',
+            transition: 'all 0.3s linear',
+            padding: '10px',
+            ':hover': {
+              borderLeft: `1px solid ${theme.palette.borders.active}`,
+            },
+          }}
+        >
+          <TokenPresentationWithPrice
+            identifier="EGLD"
+            withTokenAmount={false}
+            withTokenValue={false}
+          />
+        </Box>
+      )}
+      <span className="errorMessage">{amountError}</span>
+      {withAvailableAmount && (
+      <Text
+        fontSize={13}
+        variant="subtitle2"
+        className="availableAmount"
+      >{`${t('Available')}: ${tokenAmount} ${prettyIdentifier}`}
+      </Text>
+      )}
+    </StyledRemote.AmountWithTokenSelectionBox>
+  );
+};
+
+export default AmountInputWithTokenSelection;
