738d52e8b9fe078c42a45076cf88028fb2f42880
Alexandru Mordasan <alexandru@graffino.com>
2023-01-13 12:25:50 +0200

    fixed menu drop down overlay

--- src/components/MobileRightSidebar/styled/index.tsx
+++ src/components/MobileRightSidebar/styled/index.tsx
@@ -2,6 +2,9 @@ import { Dialog } from '@mui/material';
 import styled from 'styled-components';
 
 export const MobileRightSidebar = styled(Dialog)(({ theme: _ }) => ({
+  '.MuiBackdrop-root': {
+    backgroundColor: 'transparent',
+  },
   '.MuiDialog-container': {
     height: 'calc(100% - 110.79px - 70px) !important',
     width: '100%',
