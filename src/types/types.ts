ef948bd5bd7f79906b4188bb54f0739d4c7157d2
Rares Raulea <rares@graffino.com>
2023-01-31 00:42:36 +0200

    feat(changeOwner): define contract data interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,3 +16,23 @@ export const DelegationFunctionDescriptions: Record<
   [DelegationFunctionTitles.UnstakeTokens]: 'Unstake tokens tokens from',
   [DelegationFunctionTitles.StakeTokens]: 'Stake tokens tokens with',
 };
+
+export interface IContractData {
+  address: string;
+  balance: string;
+  code: string;
+  codeHash: string;
+  deployedAt: number;
+  developerReward: string;
+  isPayable: boolean;
+  isPayableBySmartContract: boolean;
+  isReadable: boolean;
+  isUpgradeable: boolean;
+  isVerified: boolean;
+  nonce: number;
+  ownerAddress: string;
+  rootHash: string;
+  scrCount: number;
+  shard: number;
+  txCount: number;
+}
