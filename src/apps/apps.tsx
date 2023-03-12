5d283c153bfc106c0b72363a77a05bcf0e474056
Rares Raulea <rares@graffino.com>
2023-03-12 03:21:01 +0200

    feat: add development tab on the apps page

--- src/apps/apps.tsx
+++ src/apps/apps.tsx
@@ -2,7 +2,10 @@ import MoreAppsLight from 'src/assets/img/MoreAppsLight.png';
 import MoreAppsDark from 'src/assets/img/MoreAppsDark.png';
 import { lazy } from 'react';
 import DiamondIcon from '@mui/icons-material/Diamond';
+import { RawTransactionType } from 'src/helpers/types';
+import { MultisigActionDetailed } from 'src/types/MultisigActionDetailed';
 import { withInstallGuard } from './withInstallGuard';
+import { sampleAddAction, sampleRemoveAction, sampleTransaction } from './example-app/samples';
 
 export interface AppWithRouteConfig {
   component: React.ComponentType;
@@ -16,11 +19,13 @@ export interface AppWithRouteConfig {
   imageUrlDark: any;
   isInstallable: boolean;
   title: string;
+  transaction?: RawTransactionType;
+  action?: MultisigActionDetailed;
 }
 
 export const apps: AppWithRouteConfig[] = [
   {
-    name: 'Example App',
+    name: 'My Awesome App',
     component: lazy(() =>
       import('./example-app/index').then((module) => ({
         default: module.default,
@@ -34,7 +39,45 @@ export const apps: AppWithRouteConfig[] = [
     isInstallable: true,
     icon: <DiamondIcon />,
     path: '/example-app',
-    title: 'Example App',
+    title: 'My Awesome App',
+    transaction: sampleTransaction,
+    action: sampleAddAction,
+  },
+  {
+    name: 'My Awesome App 2',
+    component: lazy(() =>
+      import('./example-app/index').then((module) => ({
+        default: module.default,
+      })),
+    ),
+    link: 'example-app-2',
+    id: 'example-app-2',
+    description: 'Congrats! You successfully created your first app for xSafe!',
+    imageUrlLight: MoreAppsLight,
+    imageUrlDark: MoreAppsDark,
+    isInstallable: true,
+    icon: <DiamondIcon />,
+    path: '/example-app-2',
+    title: 'My Awesome App 2',
+    transaction: sampleTransaction,
+    action: sampleRemoveAction,
+  },
+  {
+    name: 'My Awesome App 3',
+    component: lazy(() =>
+      import('./example-app/index').then((module) => ({
+        default: module.default,
+      })),
+    ),
+    link: 'example-app-3',
+    id: 'example-app-3',
+    description: 'Congrats! You successfully created your first app for xSafe!',
+    imageUrlLight: MoreAppsLight,
+    imageUrlDark: MoreAppsDark,
+    isInstallable: true,
+    icon: <DiamondIcon />,
+    path: '/example-app-3',
+    title: 'My Awesome App 3',
   },
 ];
 
