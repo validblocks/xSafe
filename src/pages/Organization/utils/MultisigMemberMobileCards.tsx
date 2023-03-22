9a51dad8b05bf5fb9b0d42e1ad8c7aaebc4f5289
Rares Raulea <rares@graffino.com>
2023-03-23 00:36:09 +0200

    feat: show no safe available on mobile members page

--- src/pages/Organization/utils/MultisigMemberMobileCards.tsx
+++ src/pages/Organization/utils/MultisigMemberMobileCards.tsx
@@ -10,6 +10,9 @@ import { AnchorPurple } from 'src/components/Layout/Navbar/navbar-style';
 import CopyButton from 'src/components/CopyButton';
 import SearchIcon from '@mui/icons-material/Search';
 import { network } from 'src/config';
+import NoActionsOverlay from 'src/pages/Transactions/utils/NoActionsOverlay';
+import { currentMultisigContractSelector } from 'src/redux/selectors/multisigContractsSelectors';
+import { useSelector } from 'react-redux';
 import { useOrganizationInfoContext } from '../OrganizationInfoContextProvider';
 import * as Styled from '../styled/index';
 import { useOwnerManipulationFunctions } from '.';
@@ -24,6 +27,7 @@ export const MultisigMemberMobileCards = ({ multisigMembers }: IProps) => {
   const minWidth475 = useMediaQuery('(min-width:475px)');
   const minWidth530 = useMediaQuery('(min-width:530px)');
   const { isInReadOnlyMode } = useOrganizationInfoContext();
+  const currentContract = useSelector(currentMultisigContractSelector);
 
   const {
     onRemoveMember,
@@ -36,64 +40,65 @@ export const MultisigMemberMobileCards = ({ multisigMembers }: IProps) => {
 
   return (
     <Box>
-      {multisigMembers.map((multisigMember: MultisigMember) => (
-        <Styled.MobileCardOfMembers key={multisigMember.address}>
-          <Styled.CategoryName>
-            <Typography component="span">Name</Typography>
-            <Typography fontWeight="600 !important" component="h6">{multisigMember.name}</Typography>
-          </Styled.CategoryName>
-          <Styled.CategoryName>
-            <Typography component="span">Address</Typography>
-            <Box display="flex" alignItems="center">
-              <Box
-                sx={{ borderRadius: '4px', overflow: 'hidden', marginRight: '5px' }}
-                dangerouslySetInnerHTML={{
-                  __html: toSvg(multisigMember.address, 20, { padding: 0 }),
-                }}
-              />
-              <Typography component="h6" className="mb-0 font-weight-normal">
-                {minWidth530 ? truncateInTheMiddle(multisigMember.address, 11) :
-                  minWidth475 ? truncateInTheMiddle(multisigMember.address, 5) :
-                    truncateInTheMiddle(multisigMember.address, 5)}
-              </Typography>
+      {currentContract?.address && currentContract?.address.length > 0
+        ? multisigMembers.map((multisigMember: MultisigMember) => (
+          <Styled.MobileCardOfMembers key={multisigMember.address}>
+            <Styled.CategoryName>
+              <Typography component="span">Name</Typography>
+              <Typography fontWeight="600 !important" component="h6">{multisigMember.name}</Typography>
+            </Styled.CategoryName>
+            <Styled.CategoryName>
+              <Typography component="span">Address</Typography>
               <Box display="flex" alignItems="center">
-                <CopyButton className="ml-2" link={StyledUtils.CopyIconLinkPurple} text={multisigMember.address} />
-                <AnchorPurple
-                  href={`${
-                    network.explorerAddress
-                  }/accounts/${multisigMember.address}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="ml-2"
-                >
-                  <SearchIcon />
-                </AnchorPurple>
+                <Box
+                  sx={{ borderRadius: '4px', overflow: 'hidden', marginRight: '5px' }}
+                  dangerouslySetInnerHTML={{
+                    __html: toSvg(multisigMember.address, 20, { padding: 0 }),
+                  }}
+                />
+                <Typography component="h6" className="mb-0 font-weight-normal">
+                  {minWidth530 ? truncateInTheMiddle(multisigMember.address, 11) :
+                    minWidth475 ? truncateInTheMiddle(multisigMember.address, 5) :
+                      truncateInTheMiddle(multisigMember.address, 5)}
+                </Typography>
+                <Box display="flex" alignItems="center">
+                  <CopyButton className="ml-2" link={StyledUtils.CopyIconLinkPurple} text={multisigMember.address} />
+                  <AnchorPurple
+                    href={`${
+                      network.explorerAddress
+                    }/accounts/${multisigMember.address}`}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="ml-2"
+                  >
+                    <SearchIcon />
+                  </AnchorPurple>
+                </Box>
               </Box>
-            </Box>
-          </Styled.CategoryName>
-          <Styled.CategoryName>
-            <Typography
-              component="span"
-              textAlign="left"
-              marginBottom="5px !important"
-              paddingLeft={'4px'}
-            >Actions
-            </Typography>
-            <Styled.ActionButtonsBoxMembers>
-              <Button
-                startIcon={<CustomDeleteIcon />}
-                disabled={isInReadOnlyMode}
-                onClick={() => onRemoveMember(new Address(multisigMember.address))}
-              />
-              <Button
-                startIcon={<CustomEditIcon />}
-                disabled={isInReadOnlyMode}
-                onClick={() => onEditMemberClick(multisigMember)}
-              />
-            </Styled.ActionButtonsBoxMembers>
-          </Styled.CategoryName>
-        </Styled.MobileCardOfMembers>
-      ))}
+            </Styled.CategoryName>
+            <Styled.CategoryName>
+              <Typography
+                component="span"
+                textAlign="left"
+                marginBottom="5px !important"
+                paddingLeft={'4px'}
+              >Actions
+              </Typography>
+              <Styled.ActionButtonsBoxMembers>
+                <Button
+                  startIcon={<CustomDeleteIcon />}
+                  disabled={isInReadOnlyMode}
+                  onClick={() => onRemoveMember(new Address(multisigMember.address))}
+                />
+                <Button
+                  startIcon={<CustomEditIcon />}
+                  disabled={isInReadOnlyMode}
+                  onClick={() => onEditMemberClick(multisigMember)}
+                />
+              </Styled.ActionButtonsBoxMembers>
+            </Styled.CategoryName>
+          </Styled.MobileCardOfMembers>
+        )) : <NoActionsOverlay message={'No safe available'} />}
     </Box>
   );
 };
