b2b5cfb672934ab945796b956fa589f40ec652fa
Rares Raulea <rares@graffino.com>
2023-04-20 23:08:34 +0300

    style: extract styled component for the card media container

--- src/components/NftComponent/styled/index.ts
+++ src/components/NftComponent/styled/index.ts
@@ -1,4 +1,4 @@
-import { CardContent } from '@mui/material';
+import { Box, CardContent } from '@mui/material';
 import { MainButton } from 'src/components/Theme/StyledComponents';
 import styled from 'styled-components';
 
@@ -22,5 +22,24 @@ export const SendNFTButton = styled(MainButton)(({ theme: _ }) => ({
     boxShadow: 'none !important',
     marginTop: '.35rem',
     fontSize: '13px !important',
+    position: 'relative',
+  },
+}));
+
+export const CardMediaContainer = styled(Box)(({ theme: _ }) => ({
+  '&&&': {
+    margin: '0',
+    width: '100%',
+    position: 'relative',
+    zIndex: '0',
+    '&:before': {
+      position: 'absolute',
+      content: '""',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      backgroundColor: 'rgba(76, 47, 252, 0.1)',
+    },
   },
 }));
