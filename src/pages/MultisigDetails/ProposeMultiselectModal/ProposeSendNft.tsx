e520ac421ecf03f28b02d926395fb14548e01675
Rares Raulea <rares@graffino.com>
2023-04-21 01:02:52 +0300

    feat: adjust send nft modal

--- src/pages/MultisigDetails/ProposeMultiselectModal/ProposeSendNft.tsx
+++ src/pages/MultisigDetails/ProposeMultiselectModal/ProposeSendNft.tsx
@@ -10,9 +10,12 @@ import { MultisigSendNft } from 'src/types/MultisigSendNft';
 import { useQueryClient } from 'react-query';
 import useNft from 'src/utils/useNft';
 import MemberPresentationWithPhoto from 'src/pages/Organization/MemberPresentationWithPhoto';
-import { Box, Typography, useMediaQuery } from '@mui/material';
+import { AccordionDetails, Box, Grid, Typography, useMediaQuery } from '@mui/material';
 import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks';
 import { Text } from 'src/components/StyledComponents/StyledComponents';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import * as Styled from '../../../components/Utils/styled';
 
 interface ProposeSendNftType {
@@ -119,18 +122,122 @@ const ProposeSendNft = ({
   return (
     <Box>
       <Box sx={{ p: maxWidth600 ? '16px' : '16px 48px 0.9rem' }}>
-        <Typography sx={{ mb: '0.5rem', fontWeight: 500 }}><Text>NFT name:</Text></Typography>
-        <div className="mb-3">
-          <img src={searchedNft.url} alt="" width={40} height={40} className="rounded mr-2" />
-          <Text display={'inline'}><span className="nftName">{searchedNft.name}</span></Text>
-        </div>
-        <Typography sx={{ mb: '0.5rem', fontWeight: 500 }}>
-          <Text>Sending from:</Text>
-        </Typography>
-        <MemberPresentationWithPhoto
-          memberAddress={memoizedAddress}
-          charactersLeftAfterTruncation={15}
-        />
+        {/* <Typography sx={{ mb: '0.5rem', fontWeight: 500 }}><Text>NFT name:</Text></Typography> */}
+        <Grid container className="mb-3 d-flex" spacing={2}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <img src={searchedNft.url} alt="Nft Preview" className="rounded mr-2 w-100" />
+          </Grid>
+
+        </Grid>
+        <Accordion sx={{
+          background: '#D6CFFF1A',
+          border: '1px solid #D6CFFF1A',
+          // border: '1px solid #312870',
+          color: '#fff',
+          borderRadius: '4px',
+          // background: 'rgba(76, 47, 252, 0.1)',
+          mb: 2,
+        }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: '#ddd' }} />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>More Details</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid
+              item
+              xs={12}
+            >
+              <Box>
+                <Box pb={2} display="flex" justifyContent="space-between">
+                  <Text fontWeight={600}>Token ID:</Text>
+                  <Text>{searchedNft.name}</Text>
+                </Box>
+                <Box pb={2} display="flex" justifyContent="space-between">
+                  <Text fontWeight={600} mr={1}>Royalties:</Text>
+                  <Text>{searchedNft.royalties}%</Text>
+                </Box>
+                <Box pb={2} display="flex" justifyContent="space-between">
+                  <Text fontWeight={600} mr={1}>Rank:</Text>
+                  <Text>#{searchedNft.rank}</Text>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                  <Text fontWeight={600} mr={1}>Token Type:</Text>
+                  <Text>{searchedNft.type}</Text>
+                </Box>
+              </Box>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion sx={{
+          background: '#D6CFFF1A',
+          border: '1px solid #D6CFFF1A',
+          // border: '1px solid #312870',
+          color: '#fff',
+          borderRadius: '4px',
+          // background: 'rgba(76, 47, 252, 0.1)',
+          mb: 2,
+        }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: '#ddd' }} />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Attributes</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid
+              item
+              xs={12}
+            >
+              <Box>
+                {
+                  searchedNft.metadata?.attributes?.map((attribute: {value: string, trait_type: string}) => (
+                    <Box pb={2} display="flex" justifyContent="space-between">
+                      <Text fontWeight={600}>{attribute.trait_type}:</Text>
+                      <Text overflow="hidden">{attribute.value}</Text>
+                    </Box>
+                  ))
+                }
+
+              </Box>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+
+        <Box sx={{
+          p: 2,
+          height: '100%',
+          background: '#D6CFFF1A',
+          border: '1px solid #D6CFFF1A',
+          // border: '1px solid #312870',
+          // background: 'rgba(76, 47, 252, 0.1)',
+          borderRadius: '4px',
+        }}
+        >
+
+          <Typography sx={{ mb: '0.5rem', fontWeight: 500 }}>
+            <Text>Sending from:</Text>
+          </Typography>
+          <MemberPresentationWithPhoto
+            memberAddress={memoizedAddress}
+            charactersLeftAfterTruncation={15}
+          />
+        </Box>
+
       </Box>
       <Styled.ModalDivider />
       <Box sx={{ p: maxWidth600 ? '1.25rem 16px 0' : '1.25rem 48px 0', m: ' 0 0 1rem' }}>
