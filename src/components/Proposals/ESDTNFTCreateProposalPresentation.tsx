610b03c432746216d5be9dffe20d948a40b1f248
Rares Raulea <rares@graffino.com>
2023-04-25 23:06:26 +0300

    feat: add nft proposal presentations

---
+++ src/components/Proposals/ESDTNFTCreateProposalPresentation.tsx
@@ -0,0 +1,69 @@
+import { Box } from '@mui/system';
+import React from 'react';
+import { PropertyKeyBox } from '../Utils/PropertKeyBox';
+import { PropertyValueBox } from '../Utils/PropertyValueBox';
+
+type Props = {
+  parsedArgs: {
+    attributes: {
+      tags: string[];
+      metadata: string[];
+    };
+    nftIdentifier: string;
+    nftName: string;
+    royalties: number;
+    initialQuantity: number;
+    uris: string[];
+  }
+};
+
+const ESDTNFTCreateProposalPresentation = ({ parsedArgs: {
+  attributes: {
+    tags,
+    metadata,
+  },
+  nftIdentifier,
+  nftName,
+  royalties,
+  initialQuantity,
+  uris,
+} }: Props) => (
+  <Box>
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'NFT Name'} />
+      <PropertyValueBox value={nftName} />
+    </Box>
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'NFT Identifier'} />
+      <PropertyValueBox value={nftIdentifier} />
+    </Box>
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'Royalties'} />
+      <PropertyValueBox value={`${royalties.toString()}%`} />
+    </Box>
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'Initial Quantity'} />
+      <PropertyValueBox value={initialQuantity.toString()} />
+    </Box>
+    {uris?.[0] && (
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'URI'} />
+      <PropertyValueBox value={uris?.[0]} />
+    </Box>
+    )}
+    {tags && (
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'Tags'} />
+      <PropertyValueBox value={tags?.join(', ')} />
+    </Box>
+    )}
+    {metadata && (
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'Metadata'} />
+      <PropertyValueBox value={metadata?.join(', ')} />
+    </Box>
+    )}
+  </Box>
+);
+
+export default ESDTNFTCreateProposalPresentation;
---
+++ src/components/Proposals/IssueNftCollectionProposalPresentation.tsx
@@ -0,0 +1,43 @@
+import { Box } from '@mui/system';
+import React from 'react';
+import { PropertyKeyBox } from '../Utils/PropertKeyBox';
+import { PropertyValueBox } from '../Utils/PropertyValueBox';
+
+type Props = {
+  parsedArgs: {
+    properties: Record<string, boolean>;
+    tokenTicker: string;
+    tokenName: string;
+  }
+};
+
+const IssueNftCollectionProposalPresentation = ({ parsedArgs: {
+  properties,
+  tokenTicker,
+  tokenName,
+} }: Props) => (
+  <Box>
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'Token Name'} />
+      <PropertyValueBox value={tokenName} />
+    </Box>
+    <Box>
+      <PropertyKeyBox propertyKey={'Properties'} />
+      <Box>{
+        Object.entries(properties).map(([key, value]: [string, boolean]) => (
+          <Box display="flex">
+            <PropertyKeyBox propertyKey={key} />
+            <PropertyValueBox value={value.toString()} />
+          </Box>
+        ))
+      }
+      </Box>
+    </Box>
+    <Box display="flex">
+      <PropertyKeyBox propertyKey={'Token Ticker'} />
+      <PropertyValueBox value={tokenTicker} />
+    </Box>
+  </Box>
+);
+
+export default IssueNftCollectionProposalPresentation;
