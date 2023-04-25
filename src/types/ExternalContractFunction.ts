522cca9bf46a6a6cee3d040e06337c8a7a845292
Rares Raulea <rares@graffino.com>
2023-04-25 19:24:16 +0300

    feat: add ExternalContractFunction enum

---
+++ src/types/ExternalContractFunction.ts
@@ -0,0 +1,7 @@
+export enum ExternalContractFunction {
+  ISSUE_NON_FUNGIBLE = 'issueNonFungible',
+  ISSUE_SEMI_FUNGIBLE = 'issueSemiFungible',
+  SET_SPECIAL_ROLE = 'setSpecialRole',
+  ESDT_NFT_CREATE = 'ESDTNFTCreate',
+  AUCTION_TOKEN = 'auctionToken',
+}
