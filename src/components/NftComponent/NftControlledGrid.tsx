a4e660ce37bb049ac88e82416394bea3d655e9f3
Rares Raulea <rares@graffino.com>
2023-05-05 22:15:35 +0300

    feat: add controlled grid component

---
+++ src/components/NftComponent/NftControlledGrid.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NftsByCollection from './NftsByCollection';
+import NftsUngrouped from './NftsUngrouped';
+
+interface IProps {
+    isGroupedByCollection: boolean;
+}
+
+const NftControlledGrid = ({ isGroupedByCollection }: IProps) => (
+  <div>
+    {isGroupedByCollection ? <NftsByCollection /> : <NftsUngrouped />}
+  </div>
+);
+
+export default NftControlledGrid;
