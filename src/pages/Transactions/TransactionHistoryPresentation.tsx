5bc8e591f94dd4421d6ffccde5ea77eb80641ab2
Rares Raulea <rares@graffino.com>
2023-05-24 22:53:40 +0300

    feat: transaction framer animation and hover styles

--- src/pages/Transactions/TransactionHistoryPresentation.tsx
+++ src/pages/Transactions/TransactionHistoryPresentation.tsx
@@ -2,11 +2,15 @@ import { AccordionDetails, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
 import { Text } from 'src/components/StyledComponents/StyledComponents';
-import { TransactionAccordion, TransactionAccordionSummary } from 'src/components/StyledComponents/transactions';
+import {
+  TransactionAccordion,
+  TransactionAccordionSummary,
+} from 'src/components/StyledComponents/transactions';
 import { ArrowDropDown } from '@mui/icons-material';
 import TransactionDescription from './TransactionDescription';
 import { PairOfTransactionAndDecodedAction } from './TransactionHistory';
 import TransactionSummary from './TransactionSummary';
+import { motion } from 'framer-motion';
 
 const useStyles = makeStyles(() => ({
   expanded: { margin: 0 },
@@ -38,43 +42,55 @@ const TransactionHistoryPresentation = ({
           ([transactionDate, transactionArray]: any) => (
             <div key={transactionDate}>
               <Typography variant="subtitle1" className="my-4">
-                <Text><strong>{transactionDate}</strong></Text>
+                <Text>
+                  <strong>{transactionDate}</strong>
+                </Text>
               </Typography>
               {transactionArray.map(
                 ({
                   transaction,
                   action,
-                }: PairOfTransactionAndDecodedAction) => (
-                  <TransactionAccordion
-                    key={transaction.txHash}
-                    expanded={expanded === transaction.txHash}
-                    onChange={handleChange(transaction.txHash)}
+                }: PairOfTransactionAndDecodedAction, idx: number) => (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{
+                      delay: 0.035 * idx,
+                    }}
+                    viewport={{ once: true }}
+                    exit={{ opacity: 0, scale: 0 }}
                   >
-                    <TransactionAccordionSummary
-                      expandIcon={(<ArrowDropDown />)}
-                      aria-controls="panel1a-content"
-                      className="pl-0 m-0 d-flex"
-                      classes={{
-                        content: classes.content,
-                        expanded: classes.expanded,
-                      }}
+                    <TransactionAccordion
+                      key={transaction.txHash}
+                      expanded={expanded === transaction.txHash}
+                      onChange={handleChange(transaction.txHash)}
                     >
-                      <TransactionSummary
-                        transaction={transaction}
-                        action={action}
-                      />
-                    </TransactionAccordionSummary>
+                      <TransactionAccordionSummary
+                        expandIcon={<ArrowDropDown />}
+                        aria-controls="panel1a-content"
+                        className="pl-0 m-0 d-flex"
+                        classes={{
+                          content: classes.content,
+                          expanded: classes.expanded,
+                        }}
+                      >
+                        <TransactionSummary
+                          transaction={transaction}
+                          action={action}
+                        />
+                      </TransactionAccordionSummary>
 
-                    <AccordionDetails
-                      sx={{ padding: '0', border: 'none !important' }}
-                    >
-                      <TransactionDescription
-                        transaction={transaction}
-                        action={action}
-                        signers={action?.signers}
-                      />
-                    </AccordionDetails>
-                  </TransactionAccordion>
+                      <AccordionDetails
+                        sx={{ padding: '0', border: 'none !important' }}
+                      >
+                        <TransactionDescription
+                          transaction={transaction}
+                          action={action}
+                          signers={action?.signers}
+                        />
+                      </AccordionDetails>
+                    </TransactionAccordion>
+                  </motion.div>
                 ),
               )}
             </div>
