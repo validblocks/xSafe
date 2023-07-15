e792e13b41a036e473c3b5822f4e8666f9b7c7e9
Rares Raulea <rares@graffino.com>
2023-07-15 15:57:12 +0300

    fix: mainnet import and extract common apps

--- src/apps/apps.tsx
+++ src/apps/apps.tsx
@@ -1,7 +1,6 @@
 import DiamondIcon from '@mui/icons-material/Diamond';
 import ClaimEarningsLight from 'src/assets/img/claimearnings_light.svg';
 import ClaimEarningsDark from 'src/assets/img/claimearnings_dark.svg';
-import { lazy } from 'react';
 import FileDownloadRoundedIcon from '@mui/icons-material/FileDownloadRounded';
 import { RawTransactionType } from 'src/helpers/types';
 import { MultisigActionDetailed } from 'src/types/MultisigActionDetailed';
@@ -13,6 +12,7 @@ import MoreAppsLight from 'src/assets/img/MoreAppsLight.png';
 import { sampleLendAction } from './jewelswap/samples';
 import LendInJewelSwap from './jewelswap';
 import ClaimNftAuction from './nft-auctions/index';
+import ExampleApp from './example-app/index';
 
 export interface AppWithRouteConfig {
   component: React.ComponentType;
@@ -30,96 +30,47 @@ export interface AppWithRouteConfig {
   action?: MultisigActionDetailed;
 }
 
+const commonApps = [
+  {
+    name: 'xSpotlight',
+    component: ClaimNftAuction,
+    link: 'xspotlight-claim',
+    id: 'xspotlight-claim',
+    description:
+      'Claim the resulting funds ($EGLD) from your NFT Auctions on xSpotlight.com!',
+    imageUrlLight: ClaimEarningsLight,
+    imageUrlDark: ClaimEarningsDark,
+    isInstallable: true,
+    icon: <FileDownloadRoundedIcon />,
+    path: '/xspotlight-claim',
+    title: 'xSpotlight Claim',
+  },
+  {
+    name: 'JewelSwap',
+    component: LendInJewelSwap,
+    link: 'jewelswap',
+    id: 'jewelswap',
+    description:
+      'Trade, Earn, Lend and Borrow with NFTs & $EGLD directly with JewelSwap!',
+    imageUrlLight: MoreAppsLight,
+    imageUrlDark: MoreAppsDark,
+    isInstallable: true,
+    icon: <DiamondIcon />,
+    path: '/jewelswap',
+    title: 'Jewelswap',
+    action: sampleLendAction,
+    transaction: sampleTransaction,
+  },
+];
+
 export const apps: AppWithRouteConfig[] =
   import.meta.env.VITE_MVX_ENVIRONMENT === EnvironmentsEnum.mainnet
-    ? [
-        {
-          name: 'xSpotlight',
-          component: lazy(() =>
-            import('./nft-auctions/index').then((module) => ({
-              default: module.default,
-            })),
-          ),
-          link: 'xspotlight-claim',
-          id: 'xspotlight-claim',
-          description:
-            'Claim the resulting funds ($EGLD) from your NFT Auctions on xSpotlight.com!',
-          imageUrlLight: ClaimEarningsLight,
-          imageUrlDark: ClaimEarningsDark,
-          isInstallable: true,
-          icon: <FileDownloadRoundedIcon />,
-          path: '/xspotlight-claim',
-          title: 'xSpotlight Claim',
-        },
-        {
-          name: 'JewelSwap',
-          component: lazy(() =>
-            import('./jewelswap').then((module) => ({
-              default: module.default,
-            })),
-          ),
-          link: 'jewelswap',
-          id: 'jewelswap',
-          description:
-            'Trade, Earn, Lend and Borrow with NFTs & $EGLD directly with JewelSwap!',
-          imageUrlLight: MoreAppsLight,
-          imageUrlDark: MoreAppsDark,
-          isInstallable: true,
-          icon: <DiamondIcon />,
-          path: '/jewelswap',
-          title: 'Jewelswap',
-          action: sampleLendAction,
-          transaction: sampleTransaction,
-        },
-      ]
+    ? commonApps
     : [
+        ...commonApps,
         {
-          name: 'xSpotlight',
-          // component: lazy(() =>
-          //   import('./nft-auctions/index').then((module) => ({
-          //     default: module.default,
-          //   })),
-          // ),
-          component: ClaimNftAuction,
-          link: 'xspotlight-claim',
-          id: 'xspotlight-claim',
-          description:
-            'Claim the resulting funds ($EGLD) from your NFT Auctions on xSpotlight.com!',
-          imageUrlLight: ClaimEarningsLight,
-          imageUrlDark: ClaimEarningsDark,
-          isInstallable: true,
-          icon: <FileDownloadRoundedIcon />,
-          path: '/xspotlight-claim',
-          title: 'xSpotlight Claim',
-        },
-        {
-          name: 'JewelSwap',
-          // component: lazy(() =>
-          //   import('./jewelswap').then((module) => ({
-          //     default: module.default,
-          //   })),
-          // ),
-          component: LendInJewelSwap,
-          link: 'jewelswap',
-          id: 'jewelswap',
-          description:
-            'Trade, Earn, Lend and Borrow with NFTs & $EGLD directly with JewelSwap!',
-          imageUrlLight: MoreAppsLight,
-          imageUrlDark: MoreAppsDark,
-          isInstallable: true,
-          icon: <DiamondIcon />,
-          path: '/jewelswap',
-          title: 'Jewelswap',
-          action: sampleLendAction,
-          transaction: sampleTransaction,
-        },
-        {
-          name: 'My Awesome App 2',
-          component: lazy(() =>
-            import('./example-app/index').then((module) => ({
-              default: module.default,
-            })),
-          ),
+          name: 'My Awesome App',
+          component: ExampleApp,
           link: 'example-app-2',
           id: 'example-app-2',
           description:
@@ -133,25 +84,8 @@ export const apps: AppWithRouteConfig[] =
           transaction: sampleTransaction,
           action: sampleRemoveAction,
         },
-        {
-          name: 'My Awesome App 3',
-          component: lazy(() =>
-            import('./example-app/index').then((module) => ({
-              default: module.default,
-            })),
-          ),
-          link: 'example-app-3',
-          id: 'example-app-3',
-          description:
-            'Congrats! You successfully created your first app for xSafe!',
-          imageUrlLight: MoreAppsLight,
-          imageUrlDark: MoreAppsDark,
-          isInstallable: true,
-          icon: <DiamondIcon />,
-          path: '/example-app-3',
-          title: 'My Awesome App 3',
-        },
       ];
+
 
 export const appsWithRouteConfig = apps.map((app) => ({
   ...app,
