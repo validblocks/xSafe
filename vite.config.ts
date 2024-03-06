b70966bdd4d8c979d3b577ed3050ccca0394a64e
Rares Raulea <raresraulea2000@gmail.com>
2024-03-07 00:56:04 +0200

    feat: add mkcert SSL cert for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import mkcert from 'vite-plugin-mkcert';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: 'public',
-  plugins: [react()],
+  plugins: [react(), mkcert(), eslint()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: [
-      { find: 'src', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: [{ find: 'src', replacement: path.resolve(__dirname, 'src') }],
   },
 });
