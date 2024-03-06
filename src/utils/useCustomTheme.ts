e2fbdede7a0c77af1d4383f69017d8af10c8360e
Rares Raulea <raresraulea2000@gmail.com>
2024-03-07 00:28:10 +0200

    refactor:  delete old hooks

--- src/utils/useCustomTheme.ts
+++
@@ -1,3 +0,0 @@
-import { useTheme } from "styled-components";
-
-export const useCustomTheme = () => useTheme() as any;
--- src/utils/usePrevious.ts
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect } from 'react';
-
-export const usePrevious = (value: any, initialValue: any) => {
-  const ref = useRef(initialValue);
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-};
