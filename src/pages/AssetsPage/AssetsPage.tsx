90d352f70416a38c2df950cdfbdce778c6c063c3
Rares Raulea <raresraulea2000@gmail.com>
2024-04-12 12:15:31 +0300

    refactor: extract AssetsPage

--- src/pages/AssetsPage/AssetsPage.tsx
+++ src/pages/AssetsPage/AssetsPage.tsx
@@ -1,5 +1,76 @@
 import AssetsTable from 'src/components/Assets/AssetsTable';
 
-const AssetsPage = () => (<AssetsTable />);
+import { useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { currentMultisigContractSelector } from 'src/redux/selectors/multisigContractsSelectors';
+import routeNames from 'src/routes/routeNames';
+import { parseMultisigAddress } from 'src/utils/addressUtils';
+import { useEffect, useState } from 'react';
+import { useGetLoginInfo } from '@multiversx/sdk-dapp/hooks/account';
+import ContainerWithPanels from 'src/components/Utils/ContainerWithPanels';
+import { useMediaQuery } from '@mui/material';
+import NftPage from '../NftPage';
+import { MobileAssetsPage } from './MobileAssetsPage';
+
+const panels = [
+  {
+    title: 'Tokens',
+    content: <MobileAssetsPage />,
+    tab: 'tokens',
+  },
+  { title: 'NFTs', content: <NftPage />, tab: 'nfts' },
+];
+
+function AssetsPage() {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useGetLoginInfo();
+  const currentContract = useSelector(currentMultisigContractSelector);
+  const isOnMobile = useMediaQuery('(max-width: 600px)');
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const tabFromQuery = queryParams.get('tab');
+
+  useEffect(() => {
+    if (
+      !isLoggedIn ||
+      !currentContract?.address ||
+      !parseMultisigAddress(currentContract?.address)
+    ) {
+      navigate(routeNames.multisig);
+    }
+  }, [currentContract?.address, isLoggedIn, navigate]);
+
+  const [tabIndex, setTabIndex] = useState(
+    tabFromQuery &&
+      panels.findIndex((panel) => panel.tab === tabFromQuery) !== -1
+      ? panels.findIndex((panel) => panel.tab === tabFromQuery)
+      : 0,
+  );
+
+  useEffect(() => {
+    navigate(`?tab=${panels[tabIndex].tab}`);
+  }, [navigate, tabIndex]);
+
+  useEffect(() => {
+    const index = panels.findIndex((panel) => panel.tab === tabFromQuery);
+
+    if (index !== -1) {
+      setTabIndex(index);
+    }
+  }, [navigate, tabFromQuery]);
+
+  return !isOnMobile ? (
+    <AssetsTable />
+  ) : (
+    <div>
+      <ContainerWithPanels
+        panels={panels}
+        initialTabIndex={tabIndex}
+        onTabChange={(tab: number) => setTabIndex(tab)}
+      />
+    </div>
+  );
+}
 
 export default AssetsPage;
