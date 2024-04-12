cb04ef660f2d9d61208e6f4b9e1bb2b4de574ad7
Rares Raulea <raresraulea2000@gmail.com>
2024-04-12 12:13:09 +0300

    feat: add use swip custom hook

---
+++ src/hooks/useSwipe.tsx
@@ -0,0 +1,46 @@
+import { TouchEvent } from 'react';
+
+interface SwipeInput {
+  onSwipedLeft: () => void;
+  onSwipedRight: () => void;
+}
+
+interface SwipeOutput {
+  onTouchStart: (e: TouchEvent) => void;
+  onTouchMove: (e: TouchEvent) => void;
+  onTouchEnd: () => void;
+}
+
+export const useSwipe = (input: SwipeInput): SwipeOutput => {
+  let touchStart = 0;
+  let touchEnd = 0;
+
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e: TouchEvent) => {
+    touchEnd = 0;
+    touchStart = e.targetTouches[0].clientX;
+  };
+
+  const onTouchMove = (e: TouchEvent) =>
+    (touchEnd = e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe) {
+      input.onSwipedLeft();
+    }
+    if (isRightSwipe) {
+      input.onSwipedRight();
+    }
+  };
+
+  return {
+    onTouchStart,
+    onTouchMove,
+    onTouchEnd,
+  };
+};
