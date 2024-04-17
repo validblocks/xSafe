cb95a7a3cc7d7790f88e4ffb0749cf8cdec50648
Rares Raulea <raresraulea2000@gmail.com>
2024-04-17 23:29:27 +0300

    chore: update vite and install react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import mkcert from 'vite-plugin-mkcert';
@@ -7,7 +7,18 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: 'public',
-  plugins: [react(), mkcert(), eslint()],
+  plugins: [
+    react(),
+    mkcert(),
+    eslint({
+      exclude: [/virtual:/, /node_modules/],
+    }),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.ts',
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: [{ find: 'src', replacement: path.resolve(__dirname, 'src') }],
