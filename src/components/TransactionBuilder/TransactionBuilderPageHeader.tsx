ba43bb9db7c2718c7dcf0290c855bce89b6b582c
Rares Raulea <raresraulea2000@gmail.com>
2024-05-19 11:17:22 +0300

    chore: disable unused tx builder header buttons

--- src/components/TransactionBuilder/TransactionBuilderPageHeader.tsx
+++ src/components/TransactionBuilder/TransactionBuilderPageHeader.tsx
@@ -4,19 +4,19 @@ import * as Styled from './styled';
 export const TransactionBuilderPageHeader = () => {
   return (
     <>
-      <Box pb={1}>
+      <Box pb={2}>
         <Styled.TxBuilderTitle variant="h1">
           Smart Contract Interactions
         </Styled.TxBuilderTitle>
       </Box>
-      <Box pb={1} display="flex" gap={2}>
+      {/* <Box pb={1} display="flex" gap={2}>
         <Styled.TxBuilderHeaderButton>
           Choose template
         </Styled.TxBuilderHeaderButton>
         <Styled.TxBuilderHeaderButton>
           Save as template
         </Styled.TxBuilderHeaderButton>
-      </Box>
+      </Box> */}
     </>
   );
 };
