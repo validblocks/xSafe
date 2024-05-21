1784c1274a83a2cad32a05e212d430a2a8ea170c
Rares Raulea <raresraulea2000@gmail.com>
2024-05-21 23:50:42 +0300

    feat: limit drag and drop area to dnd icon and keep form data and arguments in sync when adding, deleting and editing

--- src/components/TransactionBuilder/CustomDataBuilder.tsx
+++ src/components/TransactionBuilder/CustomDataBuilder.tsx
@@ -1,19 +1,16 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faMinus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 import { Reorder } from 'framer-motion';
 import { Box } from '@mui/material';
-import { useState, useCallback } from 'react';
-import { RemoveItemsButton, MainButton } from '../Theme/StyledComponents';
+import { useState, useCallback, useEffect } from 'react';
+import { MainButton } from '../Theme/StyledComponents';
 import { useCustomTranslation } from 'src/hooks/useCustomTranslation';
 import { CustomTextInput } from '../Utils/styled';
 import { Text } from 'src/components/StyledComponents/StyledComponents';
 import { useCustomTheme } from 'src/hooks/useCustomTheme';
-import { useFormData } from 'src/hooks/useFormData';
-import React from 'react';
-import { ArgumentInput } from './ArgumentInput';
+import { DraggableCustomArgument } from './DraggableCustomArgument';
+import { ValidationResults } from './helpers/validateArguments';
 
-type CustomArg = {
+export type CustomArg = {
   value: string;
   type: string;
   key: string;
@@ -29,11 +26,10 @@ interface Props<TValidationResults> {
 }
 
 export const CustomDataBuilder = React.memo(
-  <TValidationResults extends any[]>({
+  <TValidationResults extends ValidationResults>({
     validationResults,
     handleNewArgs,
     handleChangeEvent,
-    handleFormKeyChange,
     handleFunctionNameBlur,
     handleFunctionNameChange,
   }: Props<TValidationResults>) => {
@@ -42,22 +38,29 @@ export const CustomDataBuilder = React.memo(
 
     const [customArgs, setCustomArgs] = useState<CustomArg[]>([]);
     const [functionName, setFunctionName] = useState<string>('');
+    const [formData, setFormData] = useState<Record<string, string>>({});
 
-    const { formData, onFormChange } = useFormData({
-      handleFormKeyChange,
-      handleNewArgs,
-      handleChangeEvent,
-    });
+    useEffect(() => {
+      handleNewArgs?.(formData);
+    }, [formData, handleNewArgs]);
 
     const addNewArgsField = useCallback(() => {
-      setCustomArgs((oldArgs) => [
-        ...oldArgs,
-        { value: '', type: 'custom', key: Math.random().toString() },
-      ]);
+      setCustomArgs((oldArgs) => {
+        const newArgs = [
+          ...oldArgs,
+          { value: '', type: 'custom', key: `${oldArgs.length}-arg` },
+        ];
+        return newArgs;
+      });
     }, []);
 
     const removeArg = useCallback((argkey: string) => {
       setCustomArgs((oldArgs) => oldArgs.filter((arg) => arg.key !== argkey));
+      setFormData((oldFormData) => {
+        const newFormData = { ...oldFormData };
+        delete newFormData[argkey];
+        return newFormData;
+      });
     }, []);
 
     const onFunctionNameChange = useCallback(
@@ -75,125 +78,98 @@ export const CustomDataBuilder = React.memo(
       [handleFunctionNameBlur],
     );
 
-    console.log({ customArgs, validationResults });
+    const onValueChange = useCallback(
+      (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((oldForm) => ({
+          ...oldForm,
+          [key]: e.target.value,
+        }));
+        handleChangeEvent?.(e);
+      },
+      [handleChangeEvent],
+    );
+
+    const handleReorder = useCallback(
+      (newArgs: CustomArg[]) => {
+        setCustomArgs(newArgs);
+        const newFormData = newArgs.reduce(
+          (acc: Record<string, string>, arg: CustomArg) => {
+            acc[arg.key] = formData[arg.key] || '';
+            return acc;
+          },
+          {},
+        );
+        setFormData(newFormData);
+      },
+      [formData],
+    );
 
     return (
-      <>
-        <Box data-testid="cdb-container">
-          <Box
-            sx={{
-              borderTop: `2px solid ${theme.palette.background.default}`,
-              p: '2rem',
-            }}
+      <Box data-testid="cdb-container">
+        <Box
+          sx={{
+            borderTop: `2px solid ${theme.palette.background.default}`,
+            p: '2rem',
+          }}
+        >
+          <Text
+            sx={{ fontSize: '16px', fontWeight: 'bold' }}
+            data-testid="cdb-title"
           >
-            <Text
-              sx={{
-                fontSize: '16px',
-                fontWeight: 'bold',
-              }}
-              data-testid="cdb-title"
-            >
-              Custom Data
-            </Text>
-          </Box>
-          <Box px="2rem" pb="1rem">
-            <CustomTextInput
-              variant="outlined"
-              label={t('Function name') as string}
-              id={functionName}
-              name="functionName"
-              placeholder="Enter function name"
-              onChange={onFunctionNameChange}
-              onBlur={onFunctionNameBlur}
-              value={functionName}
-              inputProps={{ 'data-testid': 'cdb-function-name-input' }}
-            />
-          </Box>
-
-          {functionName?.length > 0 && (
-            <Box
-              sx={{
-                px: '2rem',
-                ul: {
-                  listStyle: 'none',
-                  padding: 0,
-                },
-              }}
-            >
-              <Reorder.Group values={customArgs} onReorder={setCustomArgs}>
-                {customArgs?.map((arg, idx) => (
-                  <Reorder.Item key={arg.key} value={arg} draggable>
-                    <Box>
-                      <Box display="flex" alignItems="center" gap={1}>
-                        <Box
-                          sx={{
-                            position: 'relative',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            flex: 1,
-                            mb: 1,
-                          }}
-                        >
-                          <ArgumentInput
-                            testId={`cdb-argument-${idx + 1}-text-input`}
-                            placeholder={`Argument ${idx + 1}`}
-                            label={`Argument ${idx + 1}`}
-                            value={formData[idx]}
-                            onChange={onFormChange(idx.toString())}
-                            className={
-                              validationResults?.[idx]?.isValid === false
-                                ? 'isAddressError'
-                                : ''
-                            }
-                          />
-                          <Box height={'14px'}>
-                            {validationResults?.[idx]?.isValid === false && (
-                              <Text
-                                sx={{
-                                  color: theme.palette.danger.main,
-                                  fontSize: 11,
-                                }}
-                                data-testid={`cdb-argument-${idx + 1}-error`}
-                              >
-                                {validationResults?.[idx]?.error?.reason}
-                              </Text>
-                            )}
-                          </Box>
-                        </Box>
-                        <RemoveItemsButton
-                          data-testid={`cdb-remove-argument-${idx + 1}-button`}
-                          onClick={() => removeArg(arg.key)}
-                          sx={{
-                            alignSelf: 'flex-start',
-                          }}
-                        >
-                          <FontAwesomeIcon
-                            className="mx-2"
-                            icon={faMinus as IconProp}
-                          />
-                        </RemoveItemsButton>
-                      </Box>
-                    </Box>
-                  </Reorder.Item>
-                ))}
-              </Reorder.Group>
-              <Box display="flex" gap="1.25rem">
-                <MainButton
-                  sx={{
-                    width: '100%',
-                    mb: '1rem !important',
-                    flex: 1,
-                  }}
-                  data-testid="cdb-add-argument-button"
-                  onClick={addNewArgsField}
-                >
-                  Add argument
-                </MainButton>
-              </Box>
-            </Box>
-          )}
+            Custom Data
+          </Text>
         </Box>
-      </>
+        <Box px="2rem" pb="1rem">
+          <CustomTextInput
+            variant="outlined"
+            label={t('Function name') as string}
+            id={functionName}
+            name="functionName"
+            placeholder="Enter function name"
+            onChange={onFunctionNameChange}
+            onBlur={onFunctionNameBlur}
+            value={functionName}
+            inputProps={{ 'data-testid': 'cdb-function-name-input' }}
+          />
+        </Box>
+
+        {functionName?.length > 0 && (
+          <Box sx={{ px: '2rem', ul: { listStyle: 'none', padding: 0 } }}>
+            <Reorder.Group values={customArgs} onReorder={handleReorder}>
+              {customArgs?.map((arg, idx) => (
+                <DraggableCustomArgument
+                  key={arg.key}
+                  arg={arg}
+                  onValueChange={onValueChange}
+                  removeArg={removeArg}
+                  className={
+                    validationResults?.[arg.key]?.isValid === false
+                      ? 'isAddressError'
+                      : ''
+                  }
+                  testId={`cdb-argument-${idx + 1}-text-input`}
+                  value={formData[arg.key]}
+                  placeholder={`Argument ${idx + 1}`}
+                  isInvalid={validationResults?.[arg.key]?.isValid === false}
+                  errorMessage={validationResults?.[arg.key]?.error?.reason}
+                  label={`Argument ${idx + 1}`}
+                  errorMessageTestId={`cdb-argument-${idx + 1}-error`}
+                  removeButtonTestId={`cdb-remove-argument-${idx + 1}-button`}
+                />
+              ))}
+            </Reorder.Group>
+            <Box display="flex" gap="1.25rem">
+              <MainButton
+                sx={{ width: '100%', mb: '1rem !important', flex: 1 }}
+                data-testid="cdb-add-argument-button"
+                onClick={addNewArgsField}
+              >
+                Add argument
+              </MainButton>
+            </Box>
+          </Box>
+        )}
+      </Box>
     );
   },
 );
