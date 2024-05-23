a924102726612b5581ed01ea5fea9e8a40b229cc
Rares Raulea <raresraulea2000@gmail.com>
2024-05-23 16:26:25 +0300

    feat: add template interface

---
+++ src/types/templates/index.ts
@@ -0,0 +1,10 @@
+export interface Template {
+  templateName: string;
+  owner: string;
+  type: string;
+  receiver: string;
+  endpoint: string;
+  params: string[];
+  description: string;
+  value: string;
+}
